3190200ae070f17c0f1851cae2617433573dd982
DunaevMaksimVladimirovich <test_user@gmail.com>
2024-09-09 23:17:32 +0300

    fix: split to lib and main

--- lab.c
+++ lab.c
@@ -10,18 +10,3 @@ bool isAscendingSorted(const int *array, int size) {
   }
   return true; // массив отсортирован
 }
-
-// пример локального тестирования
-
-// int main() {
-//     int arr[] = {1, 2, 3, 4, 5};
-//     int size = sizeof(arr) / sizeof(arr[0]);
-
-//     if (isAscendingSorted(arr, size)) {
-//         printf("Этот массив отсортирован по возрастанию\n");
-//     } else {
-//         printf("Этот массив не отсортирован по возрастанию\n");
-//     }
-
-//     return 0;
-// }
