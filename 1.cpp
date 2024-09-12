bf4f84b6af012d058cf6dd0d14ce0d2db2b966f9
vlyrdv <61351039+vlyrdv@users.noreply.github.com>
2024-09-12 22:06:10 +0700

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -7,25 +7,21 @@ using namespace std;
 
 
 int main() {
-    string S = "((())())()";
+    string S = "))))((((";
     int L = 0;
-    int R = 0;
+    int itog = 0;
 
     for (char element : S) {
         if (element == '(') {
-            L = L + 1;
-        } else if (element == ')') {
-            R = R + 1;
+            L++;
+        } else if (element == ')' & L > 0) {
+            L--;
+            itog++;
         }
     }
+    cout << itog;
     
-    if (L > R){
-        cout << R;
-    } else if (R > L){
-        cout << L;
-    }else{
-        cout << R;
-    }
 
     return 0;
 }
+
