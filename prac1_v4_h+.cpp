34eaf145c7fc639342299a5bd646d1b9fd35ade1
IlyaKosarev <ilyusha.k06@gmail.com>
2024-09-13 12:21:17 +0300

    1 практика, 4 вариант, hard+

---
+++ prac1_v4_h+.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <string>
+using namespace std;
+
+// решение hard+ 4 вариант, 1 занятие
+// вводите массив одной строчкой
+
+int main(){
+    vector<int> A;
+    string a;
+    stringstream aa;
+    getline(cin, a);
+    aa << a;
+    while (aa >> a) A.push_back(stoi(a));
+    vector<int> A1(size(A) + 1, 0);
+
+    for (int i = 0; i < size(A); i++){
+        A1[i + 1] = A[i] + A1[i];
+    }
+
+    int minelem = A1[0], ans = A1[1];
+    for (int i = 1; i < size(A) + 1; i ++ ){
+        minelem = min(A1[i], minelem);
+        ans = max(ans, A1[i] - minelem);
+    }
+
+    cout << ans;
+}
