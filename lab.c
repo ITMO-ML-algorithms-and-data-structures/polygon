c43e03bc21a1db0653d45bb7d6a78a06f13224fd
Lena Lutsenko <157451173+lenocheck@users.noreply.github.com>
2024-09-15 06:33:50 +1000

    Update lab.c

--- lab.c
+++ lab.c
@@ -2,4 +2,14 @@
 
 using namespace std;
 
-int main() 
+int main() {
+    int a [] = {1, 3, 5, 6};
+    int s = 0;
+    for (int i = 0; i < (end(a) - begin(a)); i++) {
+        if (a[i] % 3 == 0) {
+            s += a[i];
+        }
+    }
+    cout << s;
+    return 0;
+}
