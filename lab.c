534771894cd1b92c3a59a157edfa445faa814c61
Lena Lutsenko <157451173+lenocheck@users.noreply.github.com>
2024-09-15 06:23:14 +1000

    Update lab.c

--- lab.c
+++ lab.c
@@ -2,9 +2,7 @@
 
 using namespace std;
 
-int main()
-{
-    int a[] = {1, 3, 5, 6};
-    int s = 0;
-    return 0;
+int main() { int a[] = {1, 3, 5, 6};
+            int s = 0; 
+            return 0;
 }
