e34a3154d264670c244bea1009e00f82178fa1c6
Lena Lutsenko <157451173+lenocheck@users.noreply.github.com>
2024-09-15 05:21:37 +1000

    Update 3_Elena_Lutsenko_1.cpp

--- 3_Elena_Lutsenko_1.cpp
+++ 3_Elena_Lutsenko_1.cpp
@@ -6,9 +6,9 @@ int main() {
     int a[] = {1, 3, 5, 6};
     int s = 0;
     for (int i = 0; i < 4; i++) {
-        if (a[i] % 3 == 0) {
-            s += a[i];
-        }
+    //    if (a[i] % 3 == 0) {
+    //       s += a[i];
+    //    }
     }
     cout << s;
     return 0;
