6a3f9825ee510ab9778489aaa30fc30fbb34d7bc
Lena Lutsenko <157451173+lenocheck@users.noreply.github.com>
2024-09-15 05:08:09 +1000

    Update 3_Elena_Lutsenko_1.cpp

--- 3_Elena_Lutsenko_1.cpp
+++ 3_Elena_Lutsenko_1.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
     int a[] = { 1, 3, 5, 6 };
     int s = 0;
-    for (int i = 0; i < (end(a) - begin(a)); i++) {
+    for (int i = 0; i < 4; i++) {
         if (a[i] % 3 == 0) {
             s += a[i];
         }
