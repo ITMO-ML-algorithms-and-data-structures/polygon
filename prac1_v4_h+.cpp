a9bb2f6344fdca6b99bfb05707285e630b69d794
IlyaKosarev <ilyusha.k06@gmail.com>
2024-09-15 16:10:31 +0300

    1 занятие, 4 вариант, hard+

--- prac1_v4_h+.cpp
+++ prac1_v4_h+.cpp
@@ -13,13 +13,16 @@ int main(){
     stringstream aa;
     getline(cin, a);
     aa << a;
+    // ввод массива
     while (aa >> a) A.push_back(stoi(a));
     vector<int> A1(size(A) + 1, 0);
 
+    // создание префсумм 
     for (int i = 0; i < size(A); i++){
         A1[i + 1] = A[i] + A1[i];
     }
-
+    
+    // 
     int minelem = A1[0], ans = A1[1];
     for (int i = 1; i < size(A) + 1; i ++ ){
         minelem = min(A1[i], minelem);
