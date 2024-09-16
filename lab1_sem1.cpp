a813daeaf7e31cc1a07676151e9dd61dfc43f6c2
petlya13 <133315034+petlya13@users.noreply.github.com>
2024-09-17 00:33:17 +0300

    Add files via upload

---
+++ lab1_sem1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int s;
+    cout << "Enter seeking number:";
+    cin >> s;
+    int len = 52;
+    cout << "Enter amount of numbers in vector:";
+    cin >> len;
+    vector<int> numbers(len);
+    vector<pair<int, int>> pairs;
+    cout << "Enter numbers:" << endl;
+    for (int i = 0; i < len; i++){
+        cin >> numbers[i];
+    }
+    sort(numbers.begin(), numbers.end());
+
+    for (int i = 0; i < len-1; i++)
+    {
+        for (int j = i + 1; j < len; j++) 
+        {
+            if (s == (numbers[i] + numbers[j]) and numbers[i] != numbers[i + 1])
+            {
+                pairs.push_back(make_pair(numbers[i], numbers[j]));
+            }
+        }
+    }
+
+    for (int i = 0; i < pairs.size(); i++) {
+        cout << pairs[i].first << " " << pairs[i].second << endl;
+    }
+
+}
