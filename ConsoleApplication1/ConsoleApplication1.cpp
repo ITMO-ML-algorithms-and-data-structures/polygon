6f542f740654f7023eaa8aada691b28b519ed6a5
s3polina <polyastrinadkina@gmail.com>
2024-09-16 22:40:10 +0300

    add min search

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main() {
+    const size_t LENGTH = 10;
+    int a[LENGTH]= { 1,5,56,-23,-7850,0,6896,46,-155,-1 };
+    int min = (int)-INFINITY;
+
+    for (int i = 0; i < LENGTH; i++) {
+        //std::cin >> a[i];
+        if (a[i] < min) {
+            min = a[i];
+        }
+    }
+    std::cout << min;
+    return 0;
+}
