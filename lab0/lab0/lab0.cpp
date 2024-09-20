f224fad685641ca9a2f8a57d897ac2554bbbf788
Oleg Smirnov <467510@edu.itmo.ru>
2024-09-20 20:53:38 +0300

    lab0

---
+++ lab0/lab0/lab0.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    
+    int value, numbers_count;
+    vector <int> numbers;
+    set<vector <int>> output;
+
+    cin >> value >> numbers_count;
+
+    for (int i = 0; i < numbers_count; ++i) {
+
+        int num;
+        cin >> num;
+
+        numbers.push_back(num);
+    }
+
+   
+    for (int i = 0; i < numbers_count; ++i) {
+
+        int curr_sum = 0;
+        vector <int> array_for_push;
+
+        for (int j = i; j < numbers_count; ++j) {
+
+            int curr_elem = numbers[j];
+            curr_sum += curr_elem;
+            array_for_push.push_back(numbers[j]);
+
+            if (curr_sum == value) {
+                sort(array_for_push.begin(), array_for_push.end());
+                output.insert(array_for_push);
+            }
+        }
+    }
+
+    for (vector <int> sub_array: output) {
+        for (int elem : sub_array) {
+
+            cout << elem << ' ';
+
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
