449a50a9f1782ed00ef1f83a2e453cb2635ff521
Lev Ryabkov <118137097+LevRyabkov@users.noreply.github.com>
2024-09-21 14:33:18 +0300

    Add files via upload

---
+++ 5_ryabkov_lev_lab_1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+int main() {
+    int n;
+    std::cout << " n: "; 
+    std::cin >> n;
+    int s[n];
+git 
+    for (int i = 0; i < n; i++) 
+    {
+        std::cin >> s[i];
+    }
+    int counter = 0; 
+    for (int i = 0; i < n; i++){ 
+        if (s[i] %3  == 0) 
+        {
+            counter = counter + 1;
+        }
+        }
+        std::cout << "ответ "  << counter; 
+
+    }
