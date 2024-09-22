12cec2d7f9516acd9707426654e07f4b43f0a49c
seichikk <102675010+seichikk@users.noreply.github.com>
2024-09-22 21:11:16 +0300

    J3110_Akim_Browkin #2.cpp

---
+++ J3110_Akim_Browkin #2.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int count(const vector<int>& arr, int target) {
+    int count = 0;
+    for (int element : arr) {
+        if (element == target)
+            count++;
+    }
+    return count;
+}
+
+int main() {
+    string string1, string2;
+    cin >> string1;
+    cin >> string2;
+
+    string string1_01 = "";
+    string string2_01 = "";
+
+    vector<int> arr1;
+    vector<int> arr2;
+
+    for (int i = 0; i < string1.length(); i++) {
+        arr1.push_back(string1[i]);
+    }
+    
+    for (int i = 0; i < string2.length(); i++) {
+        arr2.push_back(string2[i]);
+    }
+
+    for (int i = 0; i < arr1.size(); i++) {
+        if (count(arr1, arr1[i]) == 1)
+            string1_01.append("0");
+        else
+            string1_01.append("1");
+    }
+
+    for (int i = 0; i < arr2.size(); i++) {
+        if (count(arr2, arr2[i]) == 1)
+            string2_01.append("0");
+        else
+            string2_01.append("1");
+    }
+
+    return 0;
+}
