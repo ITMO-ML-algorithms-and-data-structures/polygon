4a5a912c0506e8d71d623ae8affbc3489c9292c9
user6778899 <96592270+user6778899@users.noreply.github.com>
2024-09-23 10:43:10 +0300

    Add files via upload

--- polindrom.cxx
+++ polindrom.cxx
@@ -1,5 +1,5 @@
 
-#include <list>
+#include <map>
 #include <iostream>
 using namespace std;
 
@@ -9,15 +9,18 @@ int main(int argc, char *argv[])
     bool flag = true;
     int array1[5] = {1,2,3,4,5};
     //int array1[5] = {5,6,7,6,5};
-    int n = 5;
-    int array2[5];
+    //int array1[4] = {4,4,4,4};
+    int n = sizeof(array1)/sizeof(array1[0]);
+    //cout<<n;
+    
+    int array2[n];
     for(int i=n-1;i>=0;i--){array2[n-i-1]=array1[i];};
     
-    for(int i=0;i<=4;i++){if(array2[i]!=array1[i]){flag = false;};};
+    for(int i=0;i<=n-1;i++){if(array2[i]!=array1[i]){flag = false;};};
     
-    cout << "\n";
+    //cout << "\n";
     
-    //for(int i = 0;i<= 4;i++){cout<<array1[i];cout<<array2[i];};
+    //for(int i = 0;i<= n-1;i++){cout<<"z";cout<<array1[i];cout<<array2[i];};
     
     if(flag==true){cout<<"polindrom";}
     else{cout<<"no";};
