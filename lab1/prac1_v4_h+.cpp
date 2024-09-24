9fc3e0e686aeb5427803b2141d0f6542acad8ed6
IlyaKosarev <ilyusha.k06@gmail.com>
2024-09-24 11:24:44 +0300

    fixed spacing in lab1

--- lab1/prac1_v4_h+.cpp
+++ lab1/prac1_v4_h+.cpp
@@ -22,22 +22,22 @@ int main(){
     aa << a;
     if (a != ""){
         // ввод массива
-    while (aa >> a) A.push_back(stoi(a));
-    vector<int> A1(size(A) + 1, 0);
+        while (aa >> a) A.push_back(stoi(a));
+        vector<int> A1(size(A) + 1, 0);
 
-    // создание префсумм 
-    for (int i = 0; i < size(A); i++){
-        A1[i + 1] = A[i] + A1[i];
-    }
-    
-    //поддержание минимального слева + поиск наибольшей разницы
-    int minelem = A1[0], ans = A1[1];
-    for (int i = 1; i < size(A) + 1; i ++ ){
-        minelem = min(A1[i], minelem);
-        ans = max(ans, A1[i] - minelem);
-    }
+        // создание префсумм 
+        for (int i = 0; i < size(A); i++){
+            A1[i + 1] = A[i] + A1[i];
+        }
+        
+        //поддержание минимального слева + поиск наибольшей разницы
+        int minelem = A1[0], ans = A1[1];
+        for (int i = 1; i < size(A) + 1; i ++ ){
+            minelem = min(A1[i], minelem);
+            ans = max(ans, A1[i] - minelem);
+        }
 
-    cout << ans;
+        cout << ans;
     }
     else cout << "error";
 }
