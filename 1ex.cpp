6b3f8db36816dfbcb93382da04625aeb68ccab44
Vitaly_Ziryanov <vittzvar@gmail.com>
2024-09-27 10:20:46 +0300

    Изменение файлов для лаб0

--- 1ex.cpp
+++ 1ex.cpp
@@ -2,15 +2,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 //Hard -
+//Найти сумму всех чисел массива, которые кратны 3
 int main() 
 {
     int n;
     long long sm = 0;
-    cin >> n;
+    cin >> n; // Ввод размера массива
     vector<int> a(n);
-    for (int i = 0; i < n; i++) cin >> a[i];
+    for (int i = 0; i < n; i++) cin >> a[i]; //Ввод элементов массива
     for (int i = 0; i < n; i++)
         if (a[i] % 3 == 0)
             sm += a[i];
-    cout << sm << endl;
+    cout << sm << endl; //Вывод суммы
 }
