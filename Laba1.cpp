1e6f7c8d16b7b68980c19eb8e332b51d6ac62fe6
DaLex-ai <dalex_official@mail.ru>
2024-09-27 12:05:44 +0300

    fix: lab

--- Laba1.cpp
+++ Laba1.cpp
@@ -3,19 +3,25 @@ using namespace std;
 
 //Найти количество пар скобок (открытая и закрытая), корректно расположенных в строке
 string str = "";
-bool open_bracket = false;
-int answer = 0;
+int answer = 0, open_bracket = 0, close_bracket = 0;
 
 int main()
 {
-   cin >> str;
-   for(int i; i < str.length(); i++){
-       if (open_bracket == false and str[i]==('('))
-           open_bracket = true;
-       if (open_bracket == true and str[i]==(')')){
-           answer++;
-           open_bracket = false;
-       }
-   }
-   return answer;
+    cin >> str;
+    for(int i; i < str.length(); i++){
+        if (str[i] == '(' && close_bracket == 0)
+            open_bracket += 1;
+        else if(str[i] == '(' && close_bracket > 0){
+            open_bracket = 1;
+            close_bracket = 0;
+        }
+       
+        if(str[i] == ')' && open_bracket == 0)
+            close_bracket += 1;
+        else if(str[i] == ')' && open_bracket > 0){
+            answer += 1;
+            open_bracket -= 1;
+        }
+    }
+    cout << answer;
 }
