f648a4784cf414360f60040bff06d7b94ca71da4
nikitamokijcuk <nik.mokijchuk@bk.ru>
2024-09-27 11:47:11 +0300

    доработал решение + добавил комментарии

--- label_enc.cpp
+++ label_enc.cpp
@@ -1,4 +1,3 @@
-#include "label_enc.h"
 #include <iostream>
 #include <vector>
 #include <unordered_map>
@@ -7,22 +6,27 @@
 #include <sstream>
 
 
-using namespace std;
-vector<int> label_encoder (string path) {
-    ifstream file(path);
+// Функция для кодирования меток (слов) в числа, которая принимает путь к файлу в качестве аргумента и возвращает вектор целых чисел
+std::vector<int> label_encoder(std::string path) {
+    std::ifstream file(path);
 
-    string line;
-    int unique_index = 0;
-    unordered_map<string, int> label_map;
-    vector<int> label;
+    std::string line; // 32 байт
+    int unique_index = 0; // 4 байта
+    std::unordered_map<std::string, int> label_map; // n * (32 + 4) байт
+    std::vector<int> label; //  24 байт + size * 4 байт
+
+    // Чтение файла
     if (getline(file, line)) {
-        istringstream stream(line);
-        string word;
+        std::istringstream stream(line);  // Создание потока для разбора строки
+        std::string word;                 // 32 байта
+
+        // Чтение слов из строки и их кодирование
         while (stream >> word) {
             if (not label_map.contains(word)) {
                 label_map[word] = unique_index + 1;
                 unique_index++;
             }
+            // Добавление закодированного значения слова в вектор
             label.push_back(label_map[word]);
         }
     }
@@ -32,9 +36,12 @@ vector<int> label_encoder (string path) {
 }
 
 int main() {
-    vector<int> label = label_encoder("test_dir/test_4.txt");
+    // Вызов функции label_encoder
+    std::vector<int> label = label_encoder("test_dir/test_4.txt");
+
+    // Вывод закодированных меток на экран
     for (int i = 0; i < label.size(); i++) {
-        cout <<label[i] << ", ";
+        std::cout << label[i] << ", ";
     }
 
     return 0;
--- test.cpp
+++ test.cpp
@@ -7,17 +7,17 @@
 #include <sstream>
 
 
-using namespace std;
-vector<int> label_encoder (string path) {
-    ifstream file(path);
 
-    string line;
+std::vector<int> label_encoder (std::string path) {
+    std::ifstream file(path);
+
+    std::string line;
     int unique_index = 0;
-    unordered_map<string, int> label_map;
-    vector<int> label;
+    std::unordered_map<std::string, int> label_map;
+    std::vector<int> label;
     if (getline(file, line)) {
-        istringstream stream(line);
-        string word;
+        std::istringstream stream(line);
+        std::string word;
         while (stream >> word) {
             if (not label_map.contains(word)) {
                 label_map[word] = unique_index + 1;
@@ -34,12 +34,12 @@ vector<int> label_encoder (string path) {
 
 using namespace std;
 void TestLabelEnc() {
-    vector<int> test_1 {1, 2, 3, 2, 4, 2, 1};
-    vector<int> test_2 {1, 2, 3, 1, 4, 5, 6, 7, 8, 9, 10, 7, 11, 12, 13, 14, 15, 14, 4, 16, 17, 7, 18, 15, 16, 16, 9, 19, 20, 21, 22, 23, 24, 12, 9, 25, 1, 26, 9, 16, 1, 27, 22, 28, 29, 30, 7, 2, 26, 31, 26, 20, 32, 2, 7, 33, 34, 17, 35, 15, 36, 6, 21, 35, 21, 14, 20, 37, 7, 22, 2, 19, 11, 18, 32, 20, 33, 10, 20, 1, 1, 4, 19, 38, 29, 12, 20, 30, 18, 4, 11, 37, 17, 35, 19, 4, 18, 6, 30, 34, 35, 38, 34, 32, 28, 32, 20, 32, 26, 33, 19, 12, 11, 27, 7, 9, 27, 37, 38, 8, 4, 32, 7, 11, 22, 25, 13, 16, 32, 4, 31, 11, 28, 30, 5, 18, 25, 11, 3, 11, 23, 21, 23, 16, 5, 5, 9, 28, 20, 1};
-    vector<int> test_3 {1, 2, 3, 4, 5, 1, 5, 6, 2, 7, 7, 1, 2, 7, 6, 8, 5, 3, 7, 1, 3, 9, 6, 7, 7, 4, 4, 6, 1, 10, 6, 6, 4, 3, 8, 10, 8, 5, 8, 3, 4, 9, 6, 10, 5, 8, 5, 2, 4, 5};
-    vector<int> test_4 {1, 1, 1, 1, 1, 2, 1, 2, 3, 3, 3, 3, 3, 1, 2, 1, 2, 2, 2, 2, 2};
-    vector<int> test_empty {};
-    vector<int> test_same {1, 1, 1, 1, 1, 1, 1};
+    std::vector<int> test_1 {1, 2, 3, 2, 4, 2, 1};
+    std::vector<int> test_2 {1, 2, 3, 1, 4, 5, 6, 7, 8, 9, 10, 7, 11, 12, 13, 14, 15, 14, 4, 16, 17, 7, 18, 15, 16, 16, 9, 19, 20, 21, 22, 23, 24, 12, 9, 25, 1, 26, 9, 16, 1, 27, 22, 28, 29, 30, 7, 2, 26, 31, 26, 20, 32, 2, 7, 33, 34, 17, 35, 15, 36, 6, 21, 35, 21, 14, 20, 37, 7, 22, 2, 19, 11, 18, 32, 20, 33, 10, 20, 1, 1, 4, 19, 38, 29, 12, 20, 30, 18, 4, 11, 37, 17, 35, 19, 4, 18, 6, 30, 34, 35, 38, 34, 32, 28, 32, 20, 32, 26, 33, 19, 12, 11, 27, 7, 9, 27, 37, 38, 8, 4, 32, 7, 11, 22, 25, 13, 16, 32, 4, 31, 11, 28, 30, 5, 18, 25, 11, 3, 11, 23, 21, 23, 16, 5, 5, 9, 28, 20, 1};
+    std::vector<int> test_3 {1, 2, 3, 4, 5, 1, 5, 6, 2, 7, 7, 1, 2, 7, 6, 8, 5, 3, 7, 1, 3, 9, 6, 7, 7, 4, 4, 6, 1, 10, 6, 6, 4, 3, 8, 10, 8, 5, 8, 3, 4, 9, 6, 10, 5, 8, 5, 2, 4, 5};
+    std::vector<int> test_4 {1, 1, 1, 1, 1, 2, 1, 2, 3, 3, 3, 3, 3, 1, 2, 1, 2, 2, 2, 2, 2};
+    std::vector<int> test_empty {};
+    std::vector<int> test_same {1, 1, 1, 1, 1, 1, 1};
     assert(label_encoder("test_dir/test.txt") == test_1);
     assert(label_encoder("test_dir/test_2.txt") == test_2);
     assert(label_encoder("test_dir/test_3.txt") == test_3);
