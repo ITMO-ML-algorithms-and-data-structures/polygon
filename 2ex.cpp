d78c6fc76ffa1649343a6b4c782ebc8015cb2376
Vitaly_Ziryanov <vittzvar@gmail.com>
2024-09-27 10:20:46 +0300

    Изменение файлов для лаб0

--- 2ex.cpp
+++ 2ex.cpp
@@ -2,17 +2,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 // Hard +
+// Найти все пары чисел в массиве, разность которых равна заданному числу
 int main()
 {
     int n;
     long long c = 0, s;
-    cin >> n >> s;
+    cin >> n >> s; //Ввод размера массива и заданное число
     vector<int> a(n, 0);
-    for (int i = 0; i < n; i++) cin >> a[i];
+    for (int i = 0; i < n; i++) cin >> a[i]; //Ввод элементов массива
     for (int i = 0; i < n; i++)
       for (int j = i + 1; j < n; j++)
         if (abs(a[i] + a[j]) == s)
           c++;
-    cout << c << endl;
+    cout << c << endl; //Вывод суммы
     return 0;
 }
