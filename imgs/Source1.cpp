d86a3734597c073aa250ce56ed234cc49a7691d9
kitti1kitt1 <evoronova1606@gmail.com>
2024-09-30 21:17:33 +0300

    Update Source1.cpp

--- imgs/Source1.cpp
+++ imgs/Source1.cpp
@@ -1,36 +1,6 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-	int k = 0;
-	cout << "vvedite kolichstvo elementov v massive \n";
-
-	cin >> k;
-	int* maciv = new int[k];
-
-	cout << "vvedite elementi massiva \n";
-	for (int i = 0; i < k; i++) {
-		int l;
-		cin >> l;
-		maciv[i] = l;
-
-	}
-
-	int y;
-	cout << "vvedite iskomoe chislo \n";
-	cin >> y;
-
-	int u = 0;
-
-	for (int o = 0; o < k; o++) {
-		if (maciv[o] == y) {
-			u = u + 1;
-		}
-
-	}
-	cout << u;
-
-
-	return 0;
-
+int main() 
+{
 }
