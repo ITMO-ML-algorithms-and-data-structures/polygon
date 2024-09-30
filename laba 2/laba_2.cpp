1aa9f3ef5ebfcddf74c8511fd965270d2c834bbc
kitti1kitt1 <evoronova1606@gmail.com>
2024-09-30 20:58:44 +0300

    laba2

--- laba 2/laba_2.cpp
+++ laba 2/laba_2.cpp
@@ -1,14 +1,18 @@
 int most_frequent_element()
 {
+	//задаем массив
 	int size = 0;
 	std::cin >> size;
 	int* arr = new int[size];
-	
+	//иницилизируем массив
 	for (int i = 0; i < size; i++) {
 		std::cin >> arr[i];
 	}
+	//создаем переменные для хранения наиболее встречемого
+	//элемента и кол-ва его вхождений в массив
 	int most_frequent_element = 0;
 	int max_count = 0;
+	//ищем наиболее встречаемый элемент массива 
 	for (int i = 0; i < size; i++) {
 		int count = 0;
 		for (int k = 0; k < size; k++) {
@@ -22,6 +26,7 @@ int most_frequent_element()
 			most_frequent_element = arr[i];
 		}
 	}
+	//находим в массиве пустые элементы и заменяем их
 	for (int i = 0; i < size; i++) {
 		if (arr[i] == 0) {
 			arr[i] = most_frequent_element;
