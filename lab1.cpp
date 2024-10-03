0bac6f4e74e1c8eac7f96d385a383546120aed6c
Infibiss <murtazalievmatvey@gmail.com>
2024-10-04 00:32:51 +0300

    google test add + code remake

--- lab1.cpp
+++ lab1.cpp
@@ -1,37 +1,73 @@
 #include <iostream>
-#include <unordered_set>
+#include <algorithm>
 #include <fstream>
+#include <string>
+#include <gtest/gtest.h>
 
 using namespace std;
 
 // Задача:
 // Удалить дублирующиеся значения из входного массива
 
-// Асимптотическая сложность этого алгоритма O(N), где N - размер массива
-// Алгоритм проходит один раз по всему массиву, а также используется хеш таблица, где каждый insert в среднем работает за O(1).
+// Асимптотическая сложность этого алгоритма O(N*logN), где N - размер массива
+// Алгоритм вводит массив и сортирует его для использования функции unique, которая делает отсортированный массив уникальным за O(n), возвращая указатель на end измененного уникального массива
 
-// По памяти O(8*K+16*2*K)=(40K), учитывая хеш таблицу с элементами long long по 8 байт, где K - количество уникальных элементов
-// Для хеш таблицы учитываются примерные значения, так как реальное количество памяти сильно зависит от компилятора и системы
+// По памяти 8*N байт, где N - размер входного массива а 8 - размер элемента long long
 
-int main(int argc, char* argv[]) {  // В функцию main передается количество аргументов и сами аргументы командной строки в виде массива указателей
-    ifstream file(argv[1]);  // Открываем файл
+vector<long long> solve(const string &filename) {  
+    ifstream file(filename);  // Открываем файл
 
     int size;
     file >> size;  // Ввод размера массива
 
-    unordered_set<long long> ans;  // Хеш таблица, которая отсекает дублирующиеся варианты
+    vector<long long> res;
 
     for (int i = 0; i < size; i++) {
         long long elem;
         file >> elem;  // Ввод очередного элемента массива
-
-        ans.insert(elem);  // Добавление в хеш таблицу (Вычисляется хеш значение элемента и добавляется в соответствующую ячейку)
+        res.push_back(elem);
     }
     
-    for (long long elem : ans) {
-        cout << elem << ' ';  // Вывод элементов
-    } cout << '\n';
+    sort(res.begin(), res.end()); // Сортируем вектор за O(N*logN)
+    res.resize(unique(res.begin(), res.end()) - res.begin()); // С помощью разности указателей получаем размер изменененного вектора и убираем дубликаты с конца массива
 
     file.close();  // Закрываем файл
-    return 0;
+    return res;
+}
+
+// Unit tests
+const string filename = "test.txt";
+
+void write_file(const vector<long long> &arr) {
+    ofstream out(filename);
+    out << arr.size() << '\n';
+    for (long long elem : arr) {
+        out << elem << ' ';
+    }
+    out.close();
+}
+
+TEST(DublicateTest, Test0) {
+    write_file({});
+    EXPECT_EQ(solve(filename), vector<long long>({}));
+}
+
+TEST(DublicateTest, Test1) {
+    write_file({1, 2, 3, 4, 5});
+    EXPECT_EQ(solve(filename), vector<long long>({1, 2, 3, 4, 5}));
+}
+
+TEST(DublicateTest, Test2) {
+    write_file({1, 1, 1, 2, 3, 3, 4, 4, 4, 5});
+    EXPECT_EQ(solve(filename), vector<long long>({1, 2, 3, 4, 5}));
+}
+
+TEST(DublicateTest, Test3) {
+    write_file({3, 1, 2, 3, 2, 3});
+    EXPECT_EQ(solve(filename), vector<long long>({1, 2, 3}));
+}
+
+int main(int argc, char* argv[]) { // В функцию main передается количество аргументов и сами аргументы командной строки в виде массива указателей
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
