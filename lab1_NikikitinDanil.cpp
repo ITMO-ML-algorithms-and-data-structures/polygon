a4afb0638ef7b80b573154e1351219dc3cdc0a55
amor0506fati <unikitina60@gmail.com>
2024-10-04 10:51:09 +0300

    Update lab1_NikikitinDanil.cpp

--- lab1_NikikitinDanil.cpp
+++ lab1_NikikitinDanil.cpp
@@ -5,15 +5,15 @@ using namespace std;
 
 int main()
 {
-    vector <int> v = { 1,2,3,4,56,78,12,14,2,4,5,7,8,9,70,12,22,42,41 };
-    int k, max_k;
-    k = 0;
+    vector <int> arr = { 1,2,3,4,56,78,12,14,2,4,5,7,8,9,70,12,22,42,41 };
+    int len, max_len;
+    len = 0;
 
     max_k = 0;
     for (int i = 0; i < v.size(); i++) {
         if (v[i] % 2 == 0) {
-            k = k + 1;
-            max_k = max(max_k, k);
+            len = len + 1;
+            max_len = max(max_len, len);
         }
         else {
             k = 0;
