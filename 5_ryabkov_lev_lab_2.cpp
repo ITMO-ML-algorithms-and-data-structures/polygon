b51267da6132b78841ce29b4221f0a8778a3e8e6
Lev Ryabkov <118137097+LevRyabkov@users.noreply.github.com>
2024-10-04 12:21:59 +0300

    Update 5_ryabkov_lev_lab_2.cpp
    lab_2 complete

--- 5_ryabkov_lev_lab_2.cpp
+++ 5_ryabkov_lev_lab_2.cpp
@@ -5,21 +5,23 @@
 #include <fstream>
 using namespace std;
 
+
+std::unordered_set<short int> viewed; 
+std::list<short int> out_list;
+
 int main() {
 
-    unordered_set<int> viewed;
-    list<int> out_list;
-    int arr_lenght = 10000000;
-    int arr[arr_lenght];
+    short int arr_lenght = 1000000;
+    short int arr[arr_lenght];
 
     ifstream infile("dataset.txt");
-    for (int u = 0; u < arr_lenght; ++u)
+    for (short int u = 0; u < arr_lenght; ++u) 
     {
         infile >> arr[u];
     }
     infile.close();
 
-    for (int i = 0; i < arr_lenght; i++)
+    for (short int i = 0; i < arr_lenght; i++) 
     {
         if (viewed.find(arr[i]) == viewed.end())
         {
@@ -28,14 +30,15 @@ int main() {
         }
     }
 
-    vector<int> out(out_list.begin(), out_list.end());
-    cout << "новый массив: ";
+    std::vector<short int> out(out_list.begin(), out_list.end());
+    std::cout << "новый массив: ";
 
     for (size_t i = 0; i < out.size(); ++i)
     {
-        cout << out[i] << " ";
+    std::cout << out[i] << " "; 
     }
 
-    cout << endl;
+    std::cout << std::endl;
     return 0;
 }
+
