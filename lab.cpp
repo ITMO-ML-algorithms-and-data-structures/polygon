7b27de56adcfb27456fc252ae69e4ff6fdabb52d
LiahovaAleksandra <aleksandralahova54@gmail.com>
2024-10-06 21:31:54 +0300

    laba

--- lab.cpp
+++ lab.cpp
@@ -1,1 +1,19 @@
-override
+//Найти сумму всех чисел массива, которые кратны 3
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int u  = 0;
+    int one [10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    for (int i=0; i<10; i++) 
+    { 
+        if (one [i] %3 == 0){
+            u = u + one [i] ;
+        }
+                
+    }
+    cout << u;
+
+}
