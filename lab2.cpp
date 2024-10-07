b5547a106b68bc1e98d1eeb40a2636a9f3b9b9af
BystrikovDmitrii465331 <gprikson@gmail.com>
2024-10-07 12:57:11 +0300

    лаб2 сделал

---
+++ lab2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector <string> arr;
+    for (int i = 0; i < n; i++) {
+        string temp;
+        cin >> temp;
+        arr.push_back(temp);
+    }
+    cout << arr[0] << " ";
+
+    vector <int> final_arr;
+    int counter = 0;
+    std::string target = "raz";
+    for (int i = 0; i < n; i++){
+        std::string target = arr[i];
+        for (int j = 0; j < n; j++) {
+        // Check if the current element equals the target
+        // number
+        if (arr[j] == target) {
+            counter++;
+        }
+    }
+    final_arr.push_back(counter);
+    counter = 0;
+    }
+    
+    for (auto now : final_arr) {
+        cout << now << " ";
+    }
+    return 0;
+
+}
