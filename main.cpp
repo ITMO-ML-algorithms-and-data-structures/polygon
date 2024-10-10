c980569cf5277f9b8563af90cb718e46207f5ecd
AleskandrMoskovkin <moskovkin2006yandex.ru>
2024-10-10 21:13:22 +0300

    Вторая лаба по алгосам. (без юнит-тестов) + комментарии + память

--- main.cpp
+++ main.cpp
@@ -4,20 +4,22 @@ using namespace std;
 #include <vector>
 #include <fstream>
 #include <string>
+// Подключение нужных библиотек для решения задачи
 
-set<long long> mass;
-vector<int> vec;
+set<long long> mass; // Создаю множество с большими значениями | если до 1е9: примерно 315 КБ (на один элемент 8 байт + указатели по 8 байт = примерно 32 КБ)
+vector<int> vec; // Создаю вектор для вывода результата | если до 1е4: примерно 40 КБ (на один элемент 4 байта)
+// Итоговое кол-во памяти примерно 360 КБ
 
 int main() {
-    ifstream file("numbers1.txt");
-    string line;
-    while (getline(file, line)) {
-        long long m = stoll(line);
-        mass.insert(m);
+    ifstream file("numbers1.txt"); // Импортирую файл для считывания чисел
+    string line; // Считываю число как string
+    while (getline(file, line)) { // Цикл для считывания чисел из файла
+        long long m = stoll(line); // преобразую string в long long
+        mass.insert(m); // Добавляю число во множество
     }
 
-    vec.assign(mass.begin(), mass.end());
-    for (long long i : vec) {
+    vec.assign(mass.begin(), mass.end()); // Преобразуем множество в вектор
+    for (long long i : vec) { // Выводим результат
         cout << i << ", ";
     }
 }
