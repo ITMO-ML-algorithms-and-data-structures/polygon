f495be48faca55cc6487bea67a918c24b3c1cf5f
mematvey <m.emelkov@mail.ru>
2024-10-11 12:18:05 +0300

    Добавил void test, соответственно, появилась возможность тестов

--- LabelEncoder.cpp
+++ LabelEncoder.cpp
@@ -3,10 +3,11 @@
 #include <unordered_map>
 #include <sstream>
 #include <fstream>
+#include <vector>
 
 
 
-void label_encoder(const std::string& path) {
+std::vector<int> label_encoder(const std::string& path, const int& length) {
     // Мы ссылаемся на объекты в памяти, поэтому бесплатно
 
     // Создаем хэш-таблицу
@@ -19,6 +20,11 @@ void label_encoder(const std::string& path) {
 
     // Открываем файл из path
     std::ifstream File(path);
+
+    // Создаем вектор и резервируем для него место
+    // для отсутсвия реаллокаций
+    std::vector<int> returnVector;
+    returnVector.reserve(length);
 
     // Считываем значения из файла в строку и проверяем все ли ок
     if (getline(File, str)) {
@@ -36,18 +42,37 @@ void label_encoder(const std::string& path) {
 
                 ++index;
             }
-            std::cout << label_map[word] << " ";
+
+            returnVector.push_back(label_map[word]);
+
         }
     } else {
         std::cout << "Something went wrong! Check path." << '\n';
+    }
+
+    return returnVector;
+}
+
+void test(const std::vector<int>& vec, const std::vector<int>& expectedAns){
+    if (vec == expectedAns){
+        std::cout << "Everything is OK" << "\n";
+    } else {
+        std::cout << "Wrong answer!" << "\n";
     }
 }
 
 int main() {
     // N - кол-во исходных данных, avg - средняя длина строки, n - кол-во уник. знач
 
+    std::vector<int> expectedAns = {1, 2, 3, 1, 4, 2, 3, 5, 5, 6, 1, 7, 8, 9, 10, 11, 
+                                1, 1, 4, 2, 6, 7, 11, 1, 1, 12, 13, 14, 15, 16, 
+                                17, 18, 1, 2, 5, 2, 1, 6, 8};
+
     // Передаем в label_encoder() путь к входному файлу
-    label_encoder("input.txt");
+    std::vector<int> Ans = label_encoder("test.txt", expectedAns.size());
+
+    // Тестируем наш код 
+    test(Ans,expectedAns);
 
     // На выходе имеем 32 + 56 + 2 + 32 + (avg + 2) * n байт 
     
