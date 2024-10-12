1bd84e14a10bad5b02f98e3ad54f55513e17afbf
Ilya Voronin <ilyavoronin200603@gmail.com>
2024-10-12 21:12:58 +0300

    feet: Завершил лабу 2

---
+++ lab_3.cpp
@@ -0,0 +1,36 @@
+#include <iostream> 
+#include <iomanip> 
+#include <vector> 
+#include <cmath> 
+#include <string>
+#include <fstream> 
+#include <map> 
+#include <set> 
+#include <queue> 
+#include <stack> 
+#include <deque>
+
+using namespace std;
+
+int main(){
+    ifstream fin("test1.txt");
+    ofstream fein("test_result.txt");
+    
+    int n, ss = 0; fin >> n;
+    vector <string> ass_str(n);
+
+    for (int i = 0; i < n; ++i) {
+        fin >> ass_str[i];
+        if (ass_str[i] != "null")
+            ss += stoi(ass_str[i]);
+    }
+    
+    int aver = ss / n;
+    fein << aver << endl;
+    for (int i = 0; i < n; ++i){
+        if (ass_str[i] == "null")
+            ass_str[i] = to_string(aver);
+
+        fein << ass_str[i] << " ";
+    }
+}
