4c0fe49b7d8e8e2b2f621c7993e5dfd6ef09a1c0
Evgenii Kovrov <zhenia082006@gmail.com>
2024-10-14 12:56:28 +0300

    Add o notation calculation

--- lab-3/array_sampler.cpp
+++ lab-3/array_sampler.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <random>
 #include <cstdlib>  // For rand() and srand()
 #include <ctime>    // For time()
 #include <vector>
@@ -8,51 +7,25 @@
 #include <thread>
 
 std::vector<int> get_array_sample(int* array_to_sample, int array_size, int sample_size) {
-    // int sample[sample_size];
-    // std::vector<int> available_index;
-    // int tmp_index_num = array_size;
+    // Сэмплирование массива
+    std::vector<int> sample(sample_size); // O(K) для выделения памяти
+    std::vector<int> available_index(array_size); // О(N) для выделения памяти
 
-    // for(int i = 0; i < array_size; i ++) {
-    //     available_index.push_back(i);
-    // }
-
-    // for (int i = 0; i < sample_size; i ++) {
-    //     int random_index = std::rand() % tmp_index_num;
-        
-    //     sample[i] = array_to_sample[available_index[random_index]];
-
-    //     available_index.erase(available_index.begin() + random_index);
-    //     tmp_index_num --;
-    // }
-
-    std::vector<int> sample(sample_size); // This will store 3 sampled elements
-
-    // Create a vector of indices
-    std::vector<int> available_index(array_size);
-    for (int i = 0; i < array_size; ++i) {
-        available_index[i] = i; // Fill with available indices
+    for (int i = 0; i < array_size; i ++) {
+        available_index[i] = i; // О(1) * N - присваивание по индексу
     }
 
-    // Generate a random number generator with a random seed
-    // std::random_device rd;  // Obtain a random number from hardware
-    // std::mt19937 gen(rd()); // Seed the generator
+    unsigned seed = std::chrono::high_resolution_clock::now().time_since_epoch().count(); // O(1) - взятие текущего времени
+    std::srand(static_cast<unsigned>(seed)); // О(1) - передача seed в генератор
+    std::random_shuffle(available_index.begin(), available_index.end()); // О(N) - перемещивание значений в векторе
 
-    // std::shuffle(available_index.begin(), available_index.end(), gen);
-
-    // Shuffle the indices
-    // std::srand(static_cast<unsigned>(std::time(nullptr))); // Seed for randomness
-    // std::random_shuffle(available_index.begin(), available_index.end());
-
-    unsigned seed = std::chrono::high_resolution_clock::now().time_since_epoch().count();
-    std::srand(static_cast<unsigned>(seed));
-
-    std::random_shuffle(available_index.begin(), available_index.end());
-
-    // Populate the sample
-    for (int i = 0; i < sample_size; ++i) {
-        int random_index = i; // Make sure this is within bounds
-        sample[i] = array_to_sample[available_index[random_index]];
+    for (int i = 0; i < sample_size; i ++) {
+        int random_index = i; // О(1) * K - присваивание
+        sample[i] = array_to_sample[available_index[random_index]]; // (О(1) + O(1) + O(1)) * K - два взятия по индексу и одно присваивание
     }
+
+    // Сложность алгоритма составляет:
+    // О(N) в любом случае
 
     return sample;
 }
@@ -74,20 +47,12 @@ void test() {
         }
 
         std::cout << std::endl;
-
-        //std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 
 }
 
 
 int main() {
-    // int size = 4;
-    // int arr[size]= {1, 100, 10, 2};
-    // int k = 3;
-
-    // std::vector<int> result_sample = get_array_sample(arr, size, k);
-
     test();
 
     return 0;
