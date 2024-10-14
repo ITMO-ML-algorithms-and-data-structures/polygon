34fd9181a8d33f358f95d60e2954520319c67f2b
Alexandra Adrianovskaya <alexandradrianovskaya@gmail.com>
2024-10-14 14:22:59 +0300

    Лаба2+тесты

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -6,15 +6,15 @@
 
 using namespace std;
 
-//принмает на вход массив типа vector c Элементами string, на выходе массив типа вектор с элементами типа char
-vector<char> freqCount(vector<string> inputArray)
+//принимает на вход массив типа vector c Элементами string, на выходе массив типа вектор с элементами типа short
+vector<short> freqCount(vector<string> inputArray)
 {
     /*
-    1. Объявляем хэш-таблицу c ключом типа стринг и значением типа char (до 127 повторений одного слова во входном массиве, char должно хватить)
-    2. Объявляем массив outputArray типа vector c Элементами char и с размером равным размеру inputArray
+    1. Объявляем хэш-таблицу c ключом типа стринг и значением типа short (до 127 повторений одного слова во входном массиве, short должно хватить)
+    2. Объявляем массив outputArray типа vector c Элементами short и с размером равным размеру inputArray
     */
-    unordered_map<string, char> frequencyMap;
-    vector<char> outputArray (inputArray.size());
+    unordered_map<string, short> frequencyMap;
+    vector<short> outputArray (inputArray.size());
     /*
     1. Проходимся по значениям массива, помещая их в константу word типа string
     2. Увеличиваем значение на 1 по ключу word в хэш-таблице
@@ -27,7 +27,7 @@ vector<char> freqCount(vector<string> inputArray)
     Цикл: каждый элемент входящего массива проеобразуется через хэш-таблицу в частоту появлений и это значение заносится 
     в результирующий массив по соответствующему индексу
     */
-    for (int i = 0; i < inputArray.size(); i++)
+    for (short i = 0; i < (short) inputArray.size(); i++)
     {
         outputArray[i] = frequencyMap[inputArray[i]];
     }
@@ -39,20 +39,20 @@ vector<char> freqCount(vector<string> inputArray)
 void test_freqCount() 
 {
     vector<string> input_1 = {"orange", "red", "blue", "orange", "black"};
-    vector<char> expected_output_1 = {2, 1, 1, 2, 1};
-    vector<char> result_1 = freqCount(input_1);
+    vector<short> expected_output_1 = {2, 1, 1, 2, 1};
+    vector<short> result_1 = freqCount(input_1);
     assert(result_1 == expected_output_1);
     cout << "test 1 is successful" << endl;
 
     vector<string> input_2 = {"orange", "red", "blue", "orange", "black", " ", "blue", " ", "orange"};
-    vector<char> expected_output_2 = {3, 1, 2, 3, 1, 2, 2, 2, 3};
-    vector<char> result_2 = freqCount(input_2);
+    vector<short> expected_output_2 = {3, 1, 2, 3, 1, 2, 2, 2, 3};
+    vector<short> result_2 = freqCount(input_2);
     assert(result_2 == expected_output_2);
     cout << "test 2 is successful" << endl;
 
     vector<string> input_3 = {" ", " ", " ", " ", " ", " ", " ", " ", " "};
-    vector<char> expected_output_3 = {9, 9, 9, 9, 9, 9, 9, 9, 9};
-    vector<char> result_3 = freqCount(input_3);
+    vector<short> expected_output_3 = {9, 9, 9, 9, 9, 9, 9, 9, 9};
+    vector<short> result_3 = freqCount(input_3);
     assert(result_3 == expected_output_3);
     cout << "test 3 is successful" << endl;
 }
@@ -60,7 +60,7 @@ void test_freqCount()
 int main()
 {
     vector<string> input_array = {"orange", "red", "blue", "orange", "orange"};
-    vector<char> result = freqCount(input_array);
+    vector<short> result = freqCount(input_array);
     for (const short& count : result)
     {
         cout << count << " ";
