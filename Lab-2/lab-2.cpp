4d476a4ee4c97b6f1fad207ccff8433d96c3414f
DaLex-ai <dalex_official@mail.ru>
2024-10-15 21:42:07 +0300

    Лабораторная работа №2 (с дата сетом)

--- Lab-2/lab-2.cpp
+++ Lab-2/lab-2.cpp
@@ -2,34 +2,39 @@
 #include <iostream>
 #include <map>
 #include <string>
-#include <locale>
 using namespace std;
 
-string str_first, str_second; // n + n 
+string test, str_first, str_second; // n + n + n
 
 bool isomorphism(string str_first, string str_second){ //n + n 
-    if(str_first.size() != str_second.size())
-        return false; // 1
-    map<char, char> isomorf; // 48
+    if(str_first.size() != str_second.size()) //1
+        return false;
+    std::map<char, char> isomorf; // 48
     for(int i = 0; i < str_first.size(); i++)
     {
-        if(isomorf.find(str_first[i]) == isomorf.end())
-            isomorf[str_first[i]] = str_second[i]; // 1
-        else if(isomorf[str_first[i]] != str_second[i])
+        if(isomorf.find(str_first[i]) == isomorf.end()) // 1
         {
-            return false; // 1
+            isomorf[str_first[i]] = str_second[i];
+        }
+        else if(isomorf[str_first[i]] != str_second[i]) // 1
+        {
+            return false;
         }
     }
-    return true; // 1
+    return true;
 }
 
 
 int main()
 {
-    cin >> str_first >> str_second;
-    if(isomorphism(str_first, str_second))
-        cout << "true";
-    else
-        cout << "false";
+    ifstream in("test.txt");
+    while(getline(in, test)){ // k
+        str_second = str_first;
+        str_first = test;
+        if(str_first != "" and str_second != ""){
+            cout << ((isomorphism(str_first, str_second) and isomorphism(str_second, str_first)) ? "true" : "false") << endl; // 1
+            str_first = str_second = "";
+        }
+    }
 }
-// n + n + n + n + 1 + 48 + 1 + 1 + 1 = 52 + 4 * n байт
+// n + n + n + n + n + 1 + 48 + 1 + 1 + 1 + k = 52 + 5 * n + k байт
