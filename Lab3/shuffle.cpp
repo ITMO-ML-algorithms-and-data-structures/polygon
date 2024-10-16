d816ec27e3bed38fee935ef4cfda352f7a0a6feb
RaikovIlya <ilyaraikov@gmail.com>
2024-10-16 20:08:06 +0300

    feat: added comments to lab3

--- Lab3/shuffle.cpp
+++ Lab3/shuffle.cpp
@@ -6,38 +6,63 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "../Source/doctest.h"
 
-std::vector<int> shuffle(const std::string& path) {
+/*
 
-    std::ifstream file(path);
-    size_t size; file >> size;
+Алгоритм "перемешивания" элементов массива - берем последний элемент и меняем его местами со случайным элементом,
+затем берем предпоследний элемент и меняем его местами со случайным элементом, не включая последний элемент.
+Таким образом, в процессе работы алгоритма будет увеличиваться "перемешанный хвост", а по его завершении массив будет равномерно перемешан
 
-    std::random_device rd;
-    std::mt19937 gen(rd());
+*/
 
-    std::vector<int> array(size);
-    for (size_t i = 0; i < size; i++)
-        file >> array[i];
+std::vector<long long> shuffle(const std::string& path) {
 
-    for (int i = size - 1; i >= 0; i--) {
+    // Формат файла с массивом: первое число - длина массива (n), далее n строк с одним числом в каждой
 
-        std::uniform_int_distribution<> dist(0, i);
-        size_t random_index = dist(gen);
-        std::swap(array[i], array[random_index]);
+    std::ifstream file(path); // O(1) + О(1) инициализация и открытие файла
+    size_t size; file >> size; // О(1) + О(1) + О(1) выделение памяти, считывание значения, присваивание
+
+    std::random_device rd; // О(1) инициализация
+    std::mt19937 gen(rd()); // О(1) инициализация
+
+    std::vector<long long> array(size); // О(N) выделение памяти
+    for (size_t i = 0; i < size; i++) // О(1) + О(1) + О(1) + О(1) выделение памяти, присваивание, сравнение, изменение значения
+
+        // О(N) в любом случае
+
+        file >> array[i]; // О(1) + О(1) + О(1) считывание значения, взятие элемента по индексу, присваивание
+
+    for (int i = size - 1; i >= 0; i--) { // О(1) + О(1) + О(1) + О(1) выделение памяти, присваивание, сравнение, изменение значения
+
+        // О(N) в любом случае
+
+        std::uniform_int_distribution<> dist(0, i); // О(1) инициализация
+        size_t random_index = dist(gen); // О(1) + О(1) + О(1) выделение памяти, генерация числа, присваивание 
+
+        std::swap(array[i], array[random_index]); // 8 * О(1) выделение памяти на временную переменную, взятие элемента по индексу,
+                                                  // присваивание значение array[i] для этой переменной, взятие элемента по индексу х2, присваивание значение array[random_index] для array[i],
+                                                  // взятие элемента по индексу, присваивание значения временной переменной для array[random_index] 
 
     }
+
+    // Сложность алгоритма составляет О(N) в любом случае
 
     return array;
 
 }
 
-TEST_CASE("Testing the shuffle function") {
+// Тесты
+
+TEST_CASE("Testing the shuffle() function") {
 
     std::string 
-        path1 = "../Tests/shuffle/test_shuffle_1.csv",
-        path2 = "../Tests/shuffle/test_shuffle_2.csv",
-        path3 = "../Tests/shuffle/test_shuffle_3.csv";
+        path1 = "./Tests/shuffle/test_shuffle_1.csv", // Пустой файл
+        path2 = "./Tests/shuffle/test_shuffle_2.csv", // Файл с обычным массивом
+        path3 = "./Tests/shuffle/test_shuffle_3.csv", // Файл с одинаковыми числами
+        path4 = "./Tests/shuffle/test_shuffle_4.csv"; // файл с одним числом
             
-    CHECK(shuffle(path1) == std::vector<int> {});
-    CHECK(shuffle(path2) != shuffle(path2));
+    CHECK(shuffle(path1) == std::vector<long long> {});
+    CHECK(shuffle(path2) != shuffle(path2)); // Могут быть равны лишь в одном случае, который крайне маловероятен
+    CHECK(shuffle(path3) == std::vector<long long> {1, 1, 1, 1, 1, 1});
+    CHECK(shuffle(path4) == std::vector<long long> {178236});
 
 }
