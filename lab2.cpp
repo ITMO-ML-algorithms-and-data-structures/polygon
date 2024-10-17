7d08813bf39b2bb727b0e990df474b6a2d86ef2a
Infibiss <murtazalievmatvey@gmail.com>
2024-10-17 22:00:28 +0300

    fixed

--- lab2.cpp
+++ lab2.cpp
@@ -4,27 +4,32 @@
 #include <random>
 #include <unordered_set>
 #include <gtest/gtest.h>
+#include <stdexcept>
 
 using namespace std;
 
 vector<int> make_sample(vector<int>& in, int size) {
+    if (size > in.size()) // Не можем сделать семплирование нужного размера
+        throw runtime_error("Size must be bigger than vector size");;
+
     vector<int> out;
-    out.reserve(size); // Запрашивает, чтобы емкость была, по крайней мере, достаточной для содержания n элементов. Работает за O(n)
+    out.reserve(size); // Метод reserve резервирует память без инициализации элементов, имеет амортизированную временную сложность О(1)
 
     // Избавлямся от дубликатов для работы sample за O(n)
     unordered_set<int> uniq;
-    for (int elem : in)
+    for (int elem : in) // Заполнение за O(n)
         uniq.insert(elem);
-    in.clear();
+    in.clear(); // Временная сложность O(1)
     in.reserve(uniq.size());
     for (int elem : uniq)
         in.push_back(elem);
 
-    sample(in.begin(), in.end(), back_inserter(out), size, mt19937(time(0))); // Сэмплирование за O(n) c // генератором случайных чисел с начальным значением, основанным на текущем времени
+    mt19937 rnd(time(0)); // Генератор случайных чисел с начальным значением, основанным на текущем времени
+    sample(in.begin(), in.end(), back_inserter(out), size, rnd); // Сэмплирование за O(n)
     return out;
 }
 
-bool check_sample(const vector<int>& vec, int size) {
+bool check_sample(const vector<int>& vec) {
     unordered_set<int> uniq;
     for (int elem : vec)
         uniq.insert(elem);
@@ -36,7 +41,7 @@ TEST(SampleTest, Test0) {
     vector<int> in({1, 2, 3, 4});
 
     vector<int> res = make_sample(in, size);
-    EXPECT_TRUE(check_sample(res, size));
+    EXPECT_TRUE(check_sample(res));
 }
 
 TEST(SampleTest, Test1) {
@@ -44,7 +49,7 @@ TEST(SampleTest, Test1) {
     vector<int> in({1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 9, 9, 9});
 
     vector<int> res = make_sample(in, size);
-    EXPECT_TRUE(check_sample(res, size));
+    EXPECT_TRUE(check_sample(res));
 }
 
 TEST(SampleTest, Test2) {
@@ -52,7 +57,7 @@ TEST(SampleTest, Test2) {
     vector<int> in({1, 2, 2, 3});
 
     vector<int> res = make_sample(in, 2);
-    EXPECT_TRUE(check_sample(res, size));
+    EXPECT_TRUE(check_sample(res));
 }
 
 TEST(SampleTest, Test3) {
@@ -60,7 +65,30 @@ TEST(SampleTest, Test3) {
     vector<int> in({1, 1, 2, 2, 3, 3, 3});
 
     vector<int> res = make_sample(in, size);
-    EXPECT_TRUE(check_sample(res, size));
+    EXPECT_TRUE(check_sample(res));
+}
+
+TEST(SampleTest, Test4) {
+    int size = 0;
+    vector<int> in({});
+
+    vector<int> res = make_sample(in, size);
+    EXPECT_TRUE(check_sample(res));
+}
+
+TEST(SampleTest, Test5) {
+    int size = 3;
+    vector<int> in({1, 2});
+
+    EXPECT_THROW(make_sample(in, size), runtime_error);
+}
+
+TEST(SampleTest, Test6) {
+    int size = 3;
+    vector<int> in({1, 2, 3});
+
+    vector<int> res = make_sample(in, size);
+    EXPECT_TRUE(check_sample(res));
 }
 
 int main(int argc, char* argv[]) {
