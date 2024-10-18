762925f5bc859df5afbb370705b6473009467552
Shaikin Andrei <ashaikin00@mail.ru>
2024-10-18 11:35:08 +0300

    Реализован алгоритм shuffle

--- lab-3.cpp
+++ lab-3.cpp
@@ -1,21 +1,31 @@
 ﻿#include <iostream>
-#include <algorithm>
 #include <random>
 #include <vector>
 #include <cassert>
+#include <random>
 using namespace std;
+vector<int> shuffle(vector<int> vec) {
+    // Создаем генератор случайных чисел
+    random_device rd;
+    mt19937 g(rd());
 
-vector<int> shuffl(vector<int> arr) {
-    random_device rd;  // Инициализируем случайное устройство
-    mt19937 g(rd());   // Используем Mersenne Twister для генерации случайных чисел 2,5 КБ
-    shuffle(arr.begin(), arr.end(), g); // Перемешиваем массив 16 байт + элементы(shuffle не занимает больше памяти, чем начальный массив)
-    return arr;
+    // Алгоритм Фишера-Йетса
+    for (int i = vec.size() - 1; i > 0; --i) {
+        // Генерируем случайное число от 0 до i
+        uniform_int_distribution<int> dist(0, i);
+        int j = dist(g);
+
+        // Меняем местами элементы
+        swap(vec[i], vec[j]);
+    }
+    return vec;
 }
+
 int main() {
-    vector <int> original = {2, 1, 3, 4, 5, 6, 7, 8, 9, 10};
-    vector <int> shuffled = shuffl(original);
+    vector <int> original = { 2, 1, 3, 4, 5, 6, 7, 8, 9, 10 };
+    vector <int> shuffled = shuffle(original);
     assert(shuffled != original);
     vector <int> original1 = { 1000,140,232,736,69,784,98,2, 1, 3, 4, 5, 6, 7, 8, 9, 10 };
-    vector <int> shuffled1 = shuffl(original1);
+    vector <int> shuffled1 = shuffle(original1);
     assert(shuffled1 != original1);
 }
