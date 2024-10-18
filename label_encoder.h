7b4b64a4586d2f2168cd3629bf6c1827691c2588
Конькова Нина Александровна <ninakonkova19@yandex.ru>
2024-10-18 21:35:05 +0300

    +

---
+++ label_encoder.h
@@ -0,0 +1,11 @@
+#ifndef POLYGON_LABEL_ENCODER_H
+#define POLYGON_LABEL_ENCODER_H
+
+#include <vector>
+#include <string>
+#include <fstream>
+
+// Объявление функции label_encode
+std::vector<int> label_encode(std::ifstream& input);
+
+#endif // POLYGON_LABEL_ENCODER_H
