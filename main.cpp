66c71c7a73b2c0b4f48bf3824f68284639429221
Stupichev <stupicevan@gmail.com>
2024-10-18 13:03:05 +0300

    Add main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
+#include <iostream>
 #include <vector>
-
+#include <iostream>
 int index_of_number(std::vector<int> array, int number) {
     for (int i = 0; i < array.size(); i++) {
         if (array[i] == number) {
@@ -8,5 +9,10 @@ int index_of_number(std::vector<int> array, int number) {
     }
     return -1; // если number нет в array, то возвращается -1
 }
-
+int main() {
+    std::vector<int> array = {1,2,3};
+    int number = 4;
+    std::cout << index_of_number(array, number);
+    return 0;
+}
 
