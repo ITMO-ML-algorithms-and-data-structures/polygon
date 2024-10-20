02f02873f254379ff0ca250d738880ceea377c7e
Vorobyev Egor <152048362+garven20002@users.noreply.github.com>
2024-10-20 09:22:06 +0300

    Update lab2.cpp

--- lab2.cpp
+++ lab2.cpp
@@ -1,40 +1,58 @@
 #include <iostream>
 #include <vector>
-#include <unordered_map>
 
-using namespace std;
+unsigned short find_most_frequent(std::vector<unsigned short> data) { //unsigned short - 2 байта, всего n элементов сам std::vector - 24 байт, итого 2*n + 24 байт
+    unsigned short frequency[30000]; //unsigned short - 2 байта, всего 30000 элементов, итого 2*30000 = 60000 байт
+    unsigned short most_frequent_value = 0; //unsigned short - 2 байта
+    unsigned short max_count = 0; //unsigned short - 2 байта
 
-int find_max(vector<int> array) {
-    unordered_map<int, int> frequency;
-    int most_frequent = array[0], max_count = 0;
-
-    for (int num : array) {
-        if (num != NULL)
-            frequency[num]++;
-    
-        if (frequency[num] > max_count) {
-            max_count = frequency[num];
-            most_frequent = num;
+    for (unsigned short num = 0; num < data.size(); num++) {
+        if (data[num] != 0) {
+            frequency[data[num]]++; 
+            if (frequency[data[num]] > max_count) {
+                max_count = frequency[data[num]];
+                most_frequent_value = data[num];
+            }
         }
-    } 
-    return most_frequent;
-}
-
-vector<int> SimpleImputer(vector<int> array) {
-    int replace = find_max(array);
-    for (int i=0; i < array.size(); i++) {
-        if (array[i] == NULL) array[i] = replace;
     }
-    return array;
+
+    return most_frequent_value;
 }
 
-int main() {
-    vector<int> test = {1, 2, NULL, 3, NULL, 1, 5, 2, NULL, 1, NULL, 4, 7, 2, 1};
-    vector<int> new_test = SimpleImputer(test);
-    cout << "[";
-    for (int i=0; i < new_test.size(); i++)
-        if (i == new_test.size()-1) 
-            cout << new_test[i] << "]";
-        else 
-            cout << new_test[i] << ", ";
+void SimpleImputer(std::vector<unsigned short>& data) {
+    unsigned short most_frequent_value = find_most_frequent(data); //unsigned short - 2 байта
+
+    for (unsigned short& num : data) {
+        if (num == 0) {
+            num = most_frequent_value;
+        }
+    }
 }
+
+//всего выходит: 2*n + 24 + 60000 + 2 + 2 + 2 = 2*n + 60030 байт
+//можем также подсчитать память временных переменных (например num в обеих функциях), обе unsigned short, то есть 2 + 2 = 4 байта, 
+//вместе с временными переменными получаем 2*n + 60030 + 4 = 2*n + 60034 байт
+
+//теперь подсчитаем в небольшом примере
+int main() {
+    std::vector<unsigned short> data = {5, 0, 5, 0, 3, 4, 1, 0, 1, 4, 7, 0, 3, 6, 2, 9, 102, 300, 10000, 0, 1, 2, 5, 7, 1}; 
+    //тип unsigned short - 2 байта, всего 25 элементов, сам std::vector занимает 24 байта, итого: 2*25 + 24 = 74 байта
+
+    std::cout << "Before processing: ";
+    for (unsigned short num : data) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    SimpleImputer(data);
+
+    std::cout << "After processing: ";
+    for (unsigned short num : data) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
+
+//воспользуемся ранее выведенной формулой: 2*n + 60030 + 4, выходит 2*25 + 60030 + 4 = 60084 байта 
