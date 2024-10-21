a07408692ba8275941d3d2e53a7bad171de46276
Sillerus <moskovkin2006@yandex.ru>
2024-10-22 01:57:18 +0300

    Изменяю решение с использованием хеш-мапы + память и комментарии

--- main.cpp
+++ main.cpp
@@ -1,25 +1,56 @@
-using namespace std;
 #include <iostream>
-#include <set>
 #include <vector>
+#include <unordered_set>
 #include <fstream>
 #include <string>
-// Подключение нужных библиотек для решения задачи
+using namespace std;
+// подключение нужных библиотек для решения задачи
 
-set<long long> mass; // Создаю множество с большими значениями | если до 1е9: примерно 315 КБ (на один элемент 8 байт + указатели по 8 байт = примерно 32 КБ)
-vector<int> vec; // Создаю вектор для вывода результата | если до 1е4: примерно 40 КБ (на один элемент 4 байта)
-// Итоговое кол-во памяти примерно 360 КБ
-
-int main() {
-    ifstream file("numbers1.txt"); // Импортирую файл для считывания чисел
-    string line; // Считываю число как string
-    while (getline(file, line)) { // Цикл для считывания чисел из файла
-        long long m = stoll(line); // преобразую string в long long
-        mass.insert(m); // Добавляю число во множество
+void test(const vector<long long>& res, const vector<long long>& exp){
+    if(res == exp){
+        cout << "good!!\n";
     }
-
-    vec.assign(mass.begin(), mass.end()); // Преобразуем множество в вектор
-    for (long long i : vec) { // Выводим результат
-        cout << i << ", ";
+    else{
+        cout << "bad((\n";
     }
 }
+
+// функция для удаления дубликатов из вектора
+vector<long long> solution(const string& myfile) {
+    vector<long long> vec; // вектор с числами | максимум может содержать 1е9 значений => макс размер памяти 8 ГБ
+    ifstream file(myfile);
+    string line;
+    // заполнение вектора данными из файла
+    while (getline(file, line)){
+        long long a = stoll(line);
+        vec.push_back(a);
+    }
+
+    unordered_set<long long> map_seen;  // множество для отслеживания уникальных элементов | уникальных 1е4 значений => макс размер с доп данными примерно 160 КБ
+
+    size_t index = 0;  // индекс для вставки уникальных элементов
+    for (size_t i = 0; i < vec.size(); ++i) {
+        if (map_seen.find(vec[i]) == map_seen.end()) {  // если элемент еще не был встречен
+            map_seen.insert(vec[i]);  // добавляем элемент в множество
+            vec[index++] = vec[i];  // переносим уникальный элемент в начало вектора
+        }
+    }
+    // обрезаем вектор до нового размера, оставляя только уникальные элементы
+    vec.resize(index);
+    
+    return vec;
+}
+
+int main() {
+
+    vector<long long> res1 = solution("test1.txt");
+    vector<long long> exp1 = {12345, 52341, 76452, 98989};
+    test(res1, exp1);
+
+    vector<long long> res3 = solution("test3.txt");
+    vector<long long> exp3 = {};
+    test(res3, exp3);
+    
+}
+
+// итого будет где-то максимально 8 ГБ + 160 КБ + незначительные затраты типов line, size_t и буфер ввода fstream
