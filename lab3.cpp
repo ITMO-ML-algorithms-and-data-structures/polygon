2e72e2cc96f26b02500f5e11e6ce32bb443081c8
Arseniy <arsenijvagin5@gmail.com>
2024-10-22 13:18:57 +0700

    rewrite code correctly

--- lab3.cpp
+++ lab3.cpp
@@ -1,31 +1,41 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
 #include <limits>
 #include <iomanip>
+#include <vector>
 
 using namespace std;
 
-int main() {
-    ifstream inputFile("dataset.txt"); 
-    vector<float> numbers; 
-    float number;
-
-    float mini = numeric_limits<float>::max();
-    float maxi = numeric_limits<float>::lowest();
-
-    while (inputFile >> number) {
-        numbers.push_back(number);
-        if (number < mini) mini = number;
-        if (number > maxi) maxi = number;
+void find_min_max(ifstream &inputFile, float &mini, float &maxi, vector<int> &numbers) {
+    int number; // o(1)
+    while (inputFile >> number) { // o(n)
+        numbers.push_back(number); // o(1)
+        if (number < mini) mini = number; // o(1)
+        if (number > maxi) maxi = number; // o(1)
     }
-
-    cout << scientific << setprecision(1);
-    
-    for (float &num : numbers) {
-        num = (num - mini) / (maxi - mini);
-        cout << num << " ";
-    }
-    return 0;
-
 }
+
+void normalize_and_print(const vector<int> &numbers, float mini, float maxi) {
+    cout << scientific << setprecision(1); // o(1)
+    int count = 0; // o(1)
+    for (int number : numbers) { // o(n)
+        if (count >= 100) break; // o(1)
+        float normalized = (static_cast<float>(number) - mini) / (maxi - mini); // o(1)
+        cout << normalized << " "; // o(1)
+        count++; // o(1)
+    }
+}
+
+int main() {
+    ifstream inputFile("numbers10.txt"); // o(1) 
+
+    vector<int> numbers; // o(1)
+    float mini = numeric_limits<float>::max(); // o(1)
+    float maxi = numeric_limits<float>::lowest(); // o(1)
+
+    find_min_max(inputFile, mini, maxi, numbers); // o(n)
+    normalize_and_print(numbers, mini, maxi); // o(n)
+    
+    inputFile.close(); // o(1)
+    return 0;
+} // общая сложность алгоритма o(1) * 10 + o(n) * 2 = O(n)
