b7fe361b8b3eb99637edd51fc4c46ccea2f3c220
ArtemSherbackovJ3110 <artemsherbackov15@gmail.com>
2024-10-23 01:06:46 +0300

    "Добавлена 2 лаба с тестами"

---
+++ cpplib.h
@@ -0,0 +1,16 @@
+#ifndef TEMPLATE_CPPLIB_H
+#define TEMPLATE_CPPLIB_H
+
+#include <map>
+#include <string>
+#include <vector>
+
+/**
+ *  Example class used for GTest demo
+ */
+class CPPLib {
+ public:
+  bool is_isomorph(std::string input1, std::string input2);
+};
+
+#endif
