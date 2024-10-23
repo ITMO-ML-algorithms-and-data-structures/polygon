88b493c66aa9eeb347ad4b55f75b2dc872c7012b
EuGeron52 <snow.golem.mur@gmail.com>
2024-10-23 19:46:38 +0300

    сделал функцией

--- 3.hard.cpp
+++ 3.hard.cpp
@@ -1,19 +1,15 @@
 #include <iostream>
 #include <vector>
+#include <string>
 using namespace std;
-int main() {
-	int sizee;
-	cin >> sizee;
-	cout << endl;
-	vector<int> arr1(sizee);
-	for (int i = 0; i < sizee; i++) {
-		cin >> arr1[i];
-	}
+string MixArr(int sizee, const vector<int>& arr1) {
 	vector<int> arr2(sizee);
 	for (int i = 0; i < sizee; i++) {
 		arr2[(i + 1) % sizee] = arr1[i];
 	}
+	string res;
 	for (int i = 0; i < sizee; i++) {
-		cout << arr2[i] << ' ';
+		res = res + to_string(arr2[i]) + ' ';
 	}
+	return res;
 }
