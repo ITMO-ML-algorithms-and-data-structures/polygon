eaad59a1bc9ab7c56ddc46d9fb14aa74c6680f4c
kastc_z4jr90g <max.kastch@gmail.com>
2024-10-25 10:21:59 +0300

    добавил тесты

--- main.cpp
+++ main.cpp
@@ -4,9 +4,14 @@
 
 using namespace std;
 
+// int a[] = aaaaaa, ожидаемый вывод → a6 / вывод программы → a6
+// int a[] = aababaa, int len = 7: ожидаемый вывод → a2baba2 / вывод программы → a2baba2
+// int a[] = aaabbbbcccccd, int len = 6: ожидаемый вывод → a3b4c5d / вывод программы → a3b4c5d
+// int a[] = abcdeedcba, len = 4: ожидаемый исход → abcde2dcba / вывод программы → abcde2dcba
+
 int main() 
 {
-    string uncounted_str = "baaaoaarthdd";
+    string uncounted_str = "abcdeedcba";
     string result = compress(uncounted_str);
     cout << result;
 }
