e9aea5861b874d6f722dcfed1667c7f5234de9dd
kastc_z4jr90g <max.kastch@gmail.com>
2024-10-25 07:33:15 +0300

    добавил main.cpp для вызова функции с тестовой строкой

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include "lab-3.cpp"
+
+using namespace std;
+
+int main() 
+{
+    string uncounted_str = "baaaoaarthdd";
+    string result = compress(uncounted_str);
+    cout << result;
+}
