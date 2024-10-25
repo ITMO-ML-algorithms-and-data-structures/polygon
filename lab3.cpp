e2f649de08ce4376a87c5a521e2e784bdb698bc2
Vorobyev Egor <152048362+garven20002@users.noreply.github.com>
2024-10-25 11:04:59 +0300

    Update lab3.cpp

--- lab3.cpp
+++ lab3.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
 #include <string>
 
-//всего один цикл, поэтому сложность алгорита O(n)
 std::string compress_string(const std::string& str) {
-    std::string compressed_str;
-    int length = str.length();
-    compressed_str.reserve(length);
-    int count = 1;
+    std::string compressed_str; //O(1) - выделение памяти
+    int length = str.length(); //O(1) + O(1) - выделение памяти и присваивание
+    compressed_str.reserve(length); //O(1)
+    int count = 1; //O(1) + O(1) - выделение памяти и присваивание
     
-    for (int i = 1; i <= length; ++i) {
+    for (int i = 1; i <= length; ++i) { //проход по всем элементам, кроме первого, поэтому O(n-1)
         if (i < length && str[i] == str[i - 1]) {
-            ++count;
+            ++count; //O(1) - присваивание
         } else {
-            compressed_str += str[i - 1];
+            compressed_str += str[i - 1]; //O(1) + O(1) - присваивание и выбор элемента
             if (count > 1) {
-                compressed_str += std::to_string(count);
+                compressed_str += std::to_string(count); //O(1) - присваивание, std::to_string() - O(log(count)), если значение count небольшое, то сложность можно считать за O(1)
             }
             count = 1;
         }
