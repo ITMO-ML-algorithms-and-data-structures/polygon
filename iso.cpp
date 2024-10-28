0807060161ac9c5dbbc4744f5de77b6ed2856348
Haha-bip <77776358+Haha-bip@users.noreply.github.com>
2024-10-28 17:04:22 +0300

    Update iso.cpp

--- iso.cpp
+++ iso.cpp
@@ -11,14 +11,15 @@ bool Iso(const std::string& a, const std::string& b) {
     }
 
     // Создаем двe хеш-таблицы для отслеживания соответствий символов
-    std::unordered_map<char, char> one; 
+    std::unordered_map<char, char> one; //O(n) 
     std::unordered_map<char, char> reverseone;
+    // O(n) + 3 * 1 байт * 2 мапы
 
     // Итерируемся по каждому символу строк
     for (size_t i = 0; i < a.length(); ++i) {
         char charA = a[i]; 
         char charB = b[i]; 
-
+        // + 1 байт * 2
         
         if (one.count(charA)) { 
             if (one[charA] != charB) { // Проверяем, совпадает ли с charB
@@ -44,6 +45,7 @@ void runTests() {
     // Тесты с обычными значениями
     assert(Iso("egg", "add") == true);
     assert(Iso("foo", "bar") == false);
+    // (3 * 1 + 1 (за ноль) + 4 (за длину)) * 2
 
     // Пограничные значения
     assert(Iso("", "") == true); // Пустые строки
@@ -58,4 +60,7 @@ int main() {
     runTests();
     return 0;
 }
+
+// в итоге память это 2 * O(4) + 3 * 1 байт * 2 мапы + 1 байт * 2 + (3 * 1 + 1 (за ноль) + 4 (за длину)) * 2 = 32 байта
+
 
