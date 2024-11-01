cc747842286aa76f6b31384277a205aab5542371
Nikita Lesovoy <lesnik2006nik@mail.ru>
2024-11-01 04:21:19 +0300

    implemented counting values ​​in an array

---
+++ lab2 num5.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <unordered_map>
+#include <string>
+#include <vector>
+
+void print_vector(const std::vector<int> &vec) { //функция для вывода вектора
+    for (auto val : vec) { //O(N)
+        std::cout << " " << val;
+    }
+    std::cout << std::endl;
+}
+
+std::vector<int> counter(const std::string arr[], const int size){  //функция, считающая количество повторений элемента в списке
+
+    std::vector<int> out(size, 0); //O(N)
+
+    std::unordered_map<std::string, int> quantities_map; //O(1)
+
+    for(int i = 0; i < size; i++){ //O(N)
+
+        std::string word = arr[i]; //O(1+1+1) * O(N)
+
+        if (quantities_map.find(word) == quantities_map.end()){ //O(1+1+1)
+
+                quantities_map[word] = 1; //O(1+1)
+
+        }
+        
+        else{
+
+            quantities_map[word] += 1; //O(1+1)
+
+        }
+    }
+
+    for(int i = 0; i < size; i++){ //O(N)
+
+        out[i] = quantities_map[arr[i]]; //O(1+1+1)
+
+    } // заполняет вектор, заменяя каждый элемент на количество его повторений
+
+    return out;
+
+}
+
+int main(){
+
+    const std::string arr[] = {"Red", "Green", "Yellow", "Yellow", "Yellow", "Red", "White", "Black", "Black", "Yellow"};
+    const int size = sizeof(arr)/sizeof(arr[0]);
+
+    auto res_matrix = counter(arr, size);
+
+    print_vector(res_matrix);
+
+    return 0;
+
+}
