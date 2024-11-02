bf23900049d0bc1a370bc04327ba581bb3756587
Alexandra Adrianovskaya <alexandradrianovskaya@gmail.com>
2024-11-02 14:13:38 +0300

    добавила подсчет времени (лаб3)

--- lab3/lab3.cpp
+++ lab3/lab3.cpp
@@ -13,29 +13,29 @@ vector<int> freqCount(vector<string> inputArray)
     1. Объявляем хэш-таблицу c ключом типа стринг и значением типа int (до 1e9 повторений одного слова во входном массиве)
     2. Объявляем массив outputArray типа vector c Элементами int и с размером равным размеру inputArray
     */
-    unordered_map<string, int> frequencyMap;
-    vector<int> outputArray (inputArray.size());
+    unordered_map<string, int> frequencyMap; //O(1) - для объявления переменной
+    vector<int> outputArray (inputArray.size()); //O(N) - для выделения памяти
     /*
     1. Проходимся по значениям массива, помещая их в константу word типа string
     2. Увеличиваем значение на 1 по ключу word в хэш-таблице
     */
-    for (const string& word : inputArray)
+    for (const string& word : inputArray) //O(N) - цикл проходится по всем элементам inputArray
     {
-        frequencyMap[word]++;
+        frequencyMap[word]++; //O(1) - увеличение значения в хэш-таблице
     }
     /*
     Цикл: каждый элемент входящего массива проеобразуется через хэш-таблицу в частоту появлений и это значение заносится 
     в результирующий массив по соответствующему индексу
     */
-    for (int i = 0; i < (int) inputArray.size(); i++)
+    for (int i = 0; i < (int) inputArray.size(); i++) //O(N) - цикл
     {
-        outputArray[i] = frequencyMap[inputArray[i]];
+        outputArray[i] = frequencyMap[inputArray[i]]; //O(1) - доступ к элементам unordered_map
     }
     return outputArray;
-}
+} //Итог: O(N) + O(N) + O(N)= O(N)
 
 //Тесты cassert
-void test_freqCount() 
+/*void test_freqCount() 
 {
     vector<string> input_1 = {"orange", "red", "blue", "orange", "black"};
     vector<int> expected_output_1 = {2, 1, 1, 2, 1};
@@ -54,47 +54,47 @@ void test_freqCount()
     vector<int> result_3 = freqCount(input_3);
     assert(result_3 == expected_output_3);
     cout << "test 3 is successful" << endl;
-}
+}*/
 
 int main()
 {
-    vector<string> input_array;
+    vector<string> input_array; //O(1) - создание вектора
 
-    string filename = "dataset.txt";
+    string filename = "dataset.txt"; // O(1) - присваивание строки
     // Открываем файл
-    ifstream in(filename.c_str());
+    ifstream in(filename.c_str());  // O(1) - открытие файла
 
     // Считываем следующую строку из файла до конца.
-    string str;
-    while(getline(in, str)){
+    string str; // O(1) - создание строки
+    while(getline(in, str)){ // O(N) - цикл while
         // если размер строки больше 0 то пушим в массив input_array
-        if(str.size() > 0){
-            input_array.push_back(str);
+        if(str.size() > 0){ // O(1) + O(1) - размер и сравнение
+            input_array.push_back(str); // O(1) - добавление строки в вектор
         } else {
-            cerr << "Файл пуст" << endl;
+            cerr << "Файл пуст" << endl; // O(1) - вывод сообщения об ошибке
         }
     }
 
-    in.close();
+    in.close(); //O(1) - закрытие файла
 
-    vector<int> result = freqCount(input_array);
+    vector<int> result = freqCount(input_array); // O(k) - k - количество уникальных строк, зависит от реализации функции freqCount
     
     // объявляем выходной поток и открываем файл для записи    
-    ofstream out("result.txt");
+    ofstream out("result.txt"); // O(1) - открытие файла для записи
 
-    if (out.is_open())
+    if (out.is_open()) // O(1) - проверка, открыт ли файл
     {
-        for (const int& count : result)
+        for (const int& count : result) // O(k) - k - количество элементов в result
         {
-            out << count << " ";
+            out << count << " "; // O(1) для каждой записи, итого O(k)
         }
     }
-    out.close();
+    out.close(); // O(1) - закрытие файла
     
-    cout << "File has been written" << endl;
+    cout << "File has been written" << endl; // O(1) - вывод сообщения
 
     // вывод в консоль
-    
+
     // for (const int& count : result)
     // {
     //     cout << count << " ";
