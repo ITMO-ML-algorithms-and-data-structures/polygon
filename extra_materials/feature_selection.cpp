6809d30dead3abb2d7c3b8ca861f419cd36b958a
Ivan Startsev <aladush3k@mail.ru>
2024-11-06 19:11:33 +0300

    код готов

--- extra_materials/feature_selection.cpp
+++ extra_materials/feature_selection.cpp
@@ -1,23 +1,19 @@
 /*
 Как установить mlpack читать тут: https://www.mlpack.org/doc/quickstart/cpp.html
-
 Компиляция программы на mac m1:
 g++-14 -O3 -std=c++17 -I/opt/homebrew/include -L/opt/homebrew/lib feature_selection.cpp -o ./main -larmadillo -fopenmp
 */
-
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <sstream>
 #include <vector>
 #include <ctime>
-
 // библиотеки которые необходи установить. Читать тут: https://www.mlpack.org/download.html
 #define MLPACK_PRINT_INFO
 #define MLPACK_PRINT_WARN
 #include <mlpack.hpp>
 #include <mlpack/methods/linear_regression/linear_regression.hpp>
-
 // Файл в этой же папке, который я написал для задачи. Пока он обучает модель (черный ящик)
 // и возвращает метрику качества (RMSE по умолчанию)
 #include "modelling.cpp"
@@ -27,21 +23,22 @@ void generate_combi_and_train(arma::mat& dataset,float& result_score,float& best
     std::vector<int>& fichi,const std::vector<int>& nums, std::vector<int>& combination) {
     // если список удаляемых фич пуст (комбинация) или наоборот из всех фич, смысла проверять нет
     if (!combination.empty() && nums.size() != combination.size()) {
+        // создаем датасет и удаляем нужные столбцы
+        //O(n * m), где m - кол-во столбцов для удаления
+        // память 8 * (n * d) байт, где d-кол-во строк
+        arma::mat drop_dataset = drop_columns(dataset, combination);
 
-            // создаем датасет и удаляем нужные столбцы
-            arma::mat drop_dataset = drop_columns(dataset, combination);
+        //считаем метрику, искомый столбец второй с конца, айди последний
+        float score = evaluate_dataset(drop_dataset,
+            drop_dataset.n_rows - 2,
+            drop_dataset.n_rows - 1); // вроде O(n^2 * d) где d кол-во строк данных
 
-            //считаем метрику, искомый столбец второй с конца, айди последний
-            float score = evaluate_dataset(drop_columns(dataset, combination),
-                drop_dataset.n_rows - 2,
-                drop_dataset.n_rows - 1);
-
-            // проверяем подходит ли под условия задачи
-            if (-0.1 * best_score <= score - best_score && score - best_score <= best_score * 0.1
-                && fichi.size() <= combination.size()) {
-                result_score = score;
-                fichi = combination;
-            }
+        // проверяем подходит ли под условия задачи O(1)
+        if (-0.1 * best_score <= score - best_score && score - best_score <= best_score * 0.1
+            && fichi.size() <= combination.size()) {
+            result_score = score;
+            fichi = combination;
+        }
     }
     // создаем рекурсивно все возможные комбинации столбцов для удаления разной длин
     // сложность O(n^n), нормальный перебор был бы 2^n(число сочетаний)
@@ -53,7 +50,8 @@ void generate_combi_and_train(arma::mat& dataset,float& result_score,float& best
         generate_combi_and_train(dataset,result_score, best_score, fichi, nums, combination);
         combination.pop_back();
         }
-}
+} // итоговая сложность функции по времени O(n^n * n^2 * d), где n-кол-во фич датасета, а d-кол-во строк
+//итоговая сложность функции по памяти 8 * (n * m) байт для подсчета метрики, в остальном используются ссылки на главную ф-цию
 
 
 int feature_selection() {
@@ -62,14 +60,15 @@ int feature_selection() {
     const char* path = "../data/WineQT.csv";
 
     // Код, которым можно считать набор данных, который дальше требуется анализировать
+    //память o(n * d)
     arma::mat dataset;
-    if (!mlpack::data::Load(path, dataset)) {
+    if (!mlpack::data::Load(path, dataset)) { // O(n)
         // not fatal and not transpose (если false, false, mlpack::data::FileType::AutoDetect)
         throw std::runtime_error("Could not read *.csv!");
     }
 
-    std::vector<int> nums; //создаем индексы всех столбцов
-    for (int i = 0; i < dataset.n_rows - 2; i++) {
+    std::vector<int> nums; //создаем индексы всех столбцов для перебора
+    for (int i = 0; i < dataset.n_rows - 7; i++) { //O(n)
         nums.push_back(i);
     }
 
@@ -77,21 +76,22 @@ int feature_selection() {
     std::vector<int> combination;
 
     //считаем лучший результат
-    float best_score = evaluate_dataset(dataset, 11, 12);
-    // задаем значение result_score
+    float best_score = evaluate_dataset(dataset, 11, 12); //вроде O(n^2 * d) где d кол-во строк данных
+    // задаем значение result_score = 10 т.к он потом уменьшится
     float result_score = 10;
     //создаем вектор в котором будем хранить лучшую комбинацию
     std::vector<int> fichi;
     //запускаем перебор всех комбинаций
+    //сложность подсчитана в самой функции
     generate_combi_and_train(dataset, result_score, best_score,fichi, nums, combination);
 
-    //выводим все данные
+    //выводим  ответ
     std::cout << "best score: " << best_score << std::endl;
     std::cout << "result score: " << result_score << std::endl;
     std::vector<int> features;
     std::cout << "features: " << std::endl;
     // т.к на выходе у нас фичи которые мы удалили, меняем их на те которые оставляем
-    for (int i = 0; i < dataset.n_rows - 2; i++) {
+    for (int i = 0; i < dataset.n_rows - 2; i++) { // O(n)
         if (std::find(fichi.begin(), fichi.end(), i) == fichi.end() ) {
             features.push_back(i);
         }
@@ -109,7 +109,7 @@ void run_tests() {
     feature_selection();
 }
 
-//тут чисто код для для проверки правильности перебора и удаления столбцов
+//тут чисто код для для проверки правильности перебора,удаления столбцов, подсчета метрики
 void run_tests_for_modelling() {
     const char* path = "../data/WineQT.csv";
 
@@ -133,6 +133,8 @@ int main() {
     std::cout << "ВРЕМЯ РАБОТЫ: "<<(clock() - start) / CLOCKS_PER_SEC << std::endl;
     return 0;
 }
+// итоговая сложность алгоритма если взять самый сложный вычислительным момент = O(n^n * n^2 * d)
+// память в пике 2 * 8 * n * m, где m-кол-во строк в датасете
 
 
 
