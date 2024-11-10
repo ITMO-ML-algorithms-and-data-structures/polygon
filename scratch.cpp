a334d7b6e9673bcaf9fbee442cfab3e86dcb20e5
Ilya Yampolskiy <hellboy@niuitmo.ru>
2024-11-10 18:54:13 +0300

    lab-2

--- scratch.cpp
+++ scratch.cpp
@@ -1,21 +1,42 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <unordered_map>
 
-using namespace std;
+void print_vector(std::string prefix, const std::vector<int> &vec) {
+    std::cout << prefix;
+    for (auto val : vec) {
+        std::cout << " " << val;
+    }
+    std::cout << std::endl;
+}
 
-int main(){
+std::vector<int> label_encoder(const std::string arr[], const int size) {
+    std::vector<int> out(size, 0);
 
-    cout << "Size: ";
-    int len; cin >> len;
+    std::unordered_map<std::string, int> word_map;
 
-    vector <double> ass(len);
+    int counter = 0;
+    for (int i = 0; i < size; i++) {
+        std::string word = arr[i];
 
-    cout << "Numbers: ";
-    for (int i = 0; i < len; i++) cin >> ass[i];
+        if (word_map.find(word) == word_map.end()) {
+            word_map[word] = counter;
+            counter++;
+        }
 
-    sort(ass.begin(), ass.end());
+        out[i] = word_map[word];
+    }
 
-    if (len % 2 == 0) cout << (ass[len / 2] + ass[len / 2 - 1]) / 2;
-    else cout << ass[len / 2];
+    return out;
+}
+int main() {
+
+    const std::string arr[] {"red", "green", "yellow", "yellow"};
+    const int size = 4;
+
+    std::vector<int> out = label_encoder(arr, size);
+
+
+
+    return 0;
 }
