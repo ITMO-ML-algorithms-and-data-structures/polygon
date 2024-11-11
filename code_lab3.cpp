901630bdf8115c4068f9ae0f3b5b410a1cd8c3b1
introlina <119130845+introlina@users.noreply.github.com>
2024-11-11 16:35:05 +0300

    lab3 updated (fixed)

--- code_lab3.cpp
+++ code_lab3.cpp
@@ -3,10 +3,14 @@
 #include <chrono>
 #include <cassert>
 #include <unordered_map>
+#include <unordered_set>
 #include <fstream>
 #include <algorithm>
 
-/* Задача: сэмплирование (+ с большими данными до 1e9 эл-ов в массиве)*/
+/* Задача: сэмплирование */
+
+
+
 
 // (средняя врем. сложность O(1)) функция для генерации "случайного" индекса до максимального указанного
 // с помощью момента времени и лин. конг. генератора (ниже)
@@ -21,31 +25,36 @@ long int IndexRandom(long int max) {
     long int new_index = seed % max;
     if (indexes_map.find(new_index) == indexes_map.end()) { // проверяем что такого индекса ещё не встречалось
         return new_index;
-    } // O(1) в среднем тк поиск в unordered_map
+    } // O(1)
     return -1;
 }
 
 
-std::vector<long int> GoAndSample(std::vector<long int> given_massive, long int n) {
+std::vector<long int> GoAndSample(std::vector<long int> given_massive, long int n) { 
     if (n > given_massive.size()) {
         n = given_massive.size(); // проверка чтобы не выпасть из массива
     } // O(1)
-    std:: vector<long int> massive_sampled;
-    // берём n "случайных" индексов, по ним значения добавляем в arr
-    for (long int i = 0; i < n; ++i) {
-        long int new_index = IndexRandom(given_massive.size()); // генерация "случайного" индекса (O(1))
-        if (new_index > -1) { // O(1) 
-            massive_sampled.push_back(given_massive[new_index]); // O(1) 
-        }
+
+    std::vector<long int> massive_sampled;
+    std::unordered_set<long int> unique_indexes; // множество для хранения уникальных индексов
+
+    while (massive_sampled.size() < n) // O(n)
+     {
+        long int new_index = IndexRandom(given_massive.size());
+        if (new_index > -1 && unique_indexes.find(new_index) == unique_indexes.end()) { // проверяем уникальность
+            unique_indexes.insert(new_index); // O(1)
+            massive_sampled.push_back(given_massive[new_index]); // добавляем элемент в выборку
+        } // O(1)
     }
-    return massive_sampled; // O(1) + O(n), в любом случае временная сложность будет линейной 
-}
+    return massive_sampled; // возвращаем выборку
+} // O(n) общая
+
 
 // функция для проверки содержания всех элементов одного вектора в другом
 bool containsAll(const std::vector<long int>& sampledarr, const std::vector<long int>& bigarr) {
-    for (const auto& elem : sampledarr) {
+    for (const long int& elem : sampledarr) { // O(n)
         bool found = false;
-        for (const auto& sup_elem : bigarr) {
+        for (const long int& sup_elem : bigarr) { // O(m)
             if (elem == sup_elem) {
                 found = true;
                 break;
@@ -55,7 +64,7 @@ bool containsAll(const std::vector<long int>& sampledarr, const std::vector<long
             return false;
         }
     }
-    return true;
+    return true; // O(n * m), т. е. O(n**2) в худшем случае
 }
 
 
@@ -71,7 +80,6 @@ int main() {
     // Тест на отсутствие данных-----------------------
     std::vector<long int> Answer0 = GoAndSample({}, 0);
     assert(Answer0.size() == 0);
-
     // Тест на мусор в сэмплированном массиве----------
     // (+ с использованием данных из файла, где элементов > 10**4)
 
@@ -84,5 +92,16 @@ int main() {
     stream_numbers.close();
     std::vector<long int> Answer2 = GoAndSample(dataset_nums, 10010);
     assert(containsAll(Answer2, dataset_nums));
+
+    // Тест на массиве---------------------------------
+    long int m1 = 4;
+    std:: cout << std::endl;
+    std::vector<long int> Answer1 = GoAndSample({1, 2, 3, -2, 2, 9}, m);
+    for (const long int& i: Answer1){
+        std:: cout << i << " ";
+    }
     return 0;
 }
+
+
+// Итоговый подсчёт асимптотики: O(n^2)
