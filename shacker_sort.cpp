bf4fcba1709d31de2dcc8061530bf1e8a1d9186a
amor0506fati <unikitina60@gmail.com>
2024-11-17 21:31:45 +0300

    Update shacker_sort.cpp

--- shacker_sort.cpp
+++ shacker_sort.cpp
@@ -36,6 +36,20 @@ void shakerSort(std::vector<int>& arr) {
     }
 }
 
+
+//Функция для создания среднего по сложности случая (массив на половину отсортирован на половину осортирован по убыванию)
+void sortArrayHalves(std::vector<int>& arr) {
+    // Находим середину массива
+    size_t mid = arr.size() / 2;
+
+    // Сортируем первую половину (по возрастанию)
+    std::sort(arr.begin(), arr.begin() + mid);
+
+    // Сортируем вторую половину (по убыванию)
+    std::sort(arr.begin() + mid, arr.end(), std::greater<int>());
+}
+
+
 // Тесты
 int test_passed = 0;
 int test_failed = 0;
@@ -64,10 +78,12 @@ void test(std::vector<int>& arr) {
     // Копируем массив для дальнейших проверок
     std::vector<int> arr_sorted = arr;
     std::vector<int> arr_reverse = arr;
+    std::vector<int> arr_mid_reverse = arr;
 
     // Сортировка для проверки
     std::sort(arr_sorted.begin(), arr_sorted.end());
     std::sort(arr_reverse.begin(), arr_reverse.end(), std::greater<int>());
+    sortArrayHalves(arr_mid_reverse);
 
     // Тест 2: Массив уже отсортирован
     shakerSort(arr);
@@ -77,56 +93,60 @@ void test(std::vector<int>& arr) {
     shakerSort(arr_reverse);
     assertEqual(arr_reverse == arr_sorted, "Test: Array is sorted in reverse order, shakerSort should sort it");
 
+    //Тест 4: Половина массива по возрастанию ,половина по убыванию
+    shakerSort(arr_mid_reverse);
+    assertEqual(arr_mid_reverse == arr_sorted, "Test: Array is sorted in reverse on middle order, shakerSort should sort it");
     report();
 }
 
 
 
-// Функция считывающая датасет 1e4 или 1e5
-std::vector<int> load_data() {
-    std::vector<int> data;
-    std::ifstream file("1e4.txt");
-    std::string line;
-    while (std::getline(file, line)) {
-        std::istringstream ss(line);
-        std::string item;
-        while (std::getline(ss, item, '\t')) {
-            int item1 = std::stoi(item);
-            data.push_back(item1);
-        }
-    }
-    return data;
-}
+//// Функция считывающая датасет 1e4 или 1e5
+//std::vector<int> load_data() {
+//    std::vector<int> data;
+//    std::ifstream file("C:\\Users\\admin\\polygon\\1e4.txt");
+//    std::string line;
+//    while (std::getline(file, line)) {
+//        std::istringstream ss(line);
+//        std::string item;
+//        while (std::getline(ss, item, '\t')) {
+//            int item1 = std::stoi(item);
+//            data.push_back(item1);
+//        }
+//    }
+//    return data;
+//}
 
 
 
-// Функция для запуска основной программы numRuns  раз с датасетом в 1е4 и 1е5 и записи времени выполнения в файл.
-//Нужно чтоб получить данные для построения таблицы 
-void runTestsMultipleTimes(int numRuns) {
-    std::ofstream outputFile("C:\\Users\\admin\\Downloads\\execution_time.txt", std::ios::out | std::ios::trunc);
-
-    if (!outputFile) {
-        std::cerr << "Ошибка открытия файла для записи.\n";
-        return;
-    }
-
-    for (int i = 0; i < numRuns; ++i) {
-        auto start = std::chrono::high_resolution_clock::now();
-
-        // Загружаем данные
-        std::vector<int> arr = load_data();
-
-        // Вычисление времени выполнения
-        auto end = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double> diff = end - start;
-
-        // Записываем время выполнения в файл
-        outputFile << diff.count() << " \n";
-    }
-
-    outputFile.close();
-    std::cout << "Execution times have been saved to execution_times.txt.\n";
-}
+//// Функция для запуска основной программы numRuns  раз с датасетом в 1е4 и 1е5 и записи времени выполнения в файл.
+////Нужно чтоб получить данные для построения таблицы 
+//void runTestsMultipleTimes(int numRuns) {
+//    std::ofstream outputFile("C:\\Users\\admin\\Downloads\\shaker_execution_time1e4.txt", std::ios::out | std::ios::trunc);
+//
+//    if (!outputFile) {
+//        std::cerr << "Ошибка открытия файла для записи.\n";
+//        return;
+//    }
+//
+//    for (int i = 0; i < numRuns; ++i) {
+//        auto start = std::chrono::high_resolution_clock::now();
+//
+//        // Загружаем данные
+//        std::vector<int> arr = load_data();
+//        shakerSort(arr);
+//
+//        // Вычисление времени выполнения
+//        auto end = std::chrono::high_resolution_clock::now();
+//        std::chrono::duration<double> diff = end - start;
+//
+//        // Записываем время выполнения в файл
+//        outputFile << diff.count() << " \n";
+//    }
+//
+//    outputFile.close();
+//    std::cout << "Execution times have been saved to execution_times.txt.\n";
+//}
 
 
 
@@ -159,10 +179,11 @@ void processFile(const std::string& inputFile, const std::string& outputFile) {
         std::vector<int> arr1;
         while (lineStream >> number) {
             arr1.push_back(number);
-        }
+        }//Нельзя сразу инициализировать фкк так как начнет выполнение программа сортировки
         std::vector<int> arr;
         std::ranges::copy(arr1, std::back_inserter(arr));
-        //тут массив нами проинициализирован поэтому функция сортировки начнет свое выполнение и время будет верным
+        //тут массив нами проинициализирован сразу целиком поэтому функция сортировки начнет свое выполнение и время будет верным
+        shakerSort(arr);
         auto end = std::chrono::high_resolution_clock::now(); // Конец измерения времени
         std::chrono::duration<double> duration = end - start; // Вычисление времени выполнения в секундах
 
@@ -179,8 +200,8 @@ void processFile(const std::string& inputFile, const std::string& outputFile) {
 
 
 int main() {
-    // Запуск программы 50 раз и запись времени выполнения в файл
-    runTestsMultipleTimes(50);
+    //// Запуск программы 50 раз и запись времени выполнения в файл
+    //runTestsMultipleTimes(100);
 
     std::string inputFile = "C:\\Users\\admin\\Downloads\\random_numbers.txt";  // Имя входного файла
     std::string outputFile = "C:\\Users\\admin\\Downloads\\time_line_chart.txt"; // Имя выходного файла
