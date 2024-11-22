68f6afa83c332bb8ae781992ca945fef41d41895
Finzz71 <timonyakytsk@gmail.com>
2024-11-22 17:54:59 +0300

    добавил работу с файлами

--- Lab3.cpp
+++ Lab3.cpp
@@ -1,28 +1,37 @@
-#include <iostream> 
-#include <string> 
+#include <iostream>
+#include <string>
+#include <fstream>
 
 using namespace std;
 
 int main() {
-    string row = "a(b)(((a)b))";
 
-    int count_open = 0;
-    int count_close = 0;
+	ifstream input_file("test3.txt");
 
-    for (int i = 0; i < row.size(); i++) {
-        if (row[i] == '(')
-            count_open += 1;
-        if (row[i] == ')') {
-            count_close += 1;
-            if (count_close > count_open)
-                break;
-        }
-    }
-    if (count_open == count_close)
-        cout << "False";
-    else if (count_open > count_close)
-        cout << "True";
-    else
-        cout << "Error";
+	string row;
+	int value;
+
+	while (input_file >> value) {
+		row.push_back(value);
+	}
+	input_file.close();
+
+	int count_open = 0;
+	int count_close = 0;
+
+	for (int i = 0; i < row.size(); i++) {
+		if (row[i] == '(')
+			count_open += 1;
+		if (row[i] == ')') {
+			count_close += 1;
+			if (count_close > count_open)
+				break;
+		}
+	}
+	if (count_open == count_close)
+		cout << "False";
+	else if (count_open > count_close)
+		cout << "True";
+	else
+		cout << "Error";
 }
-//Сложность - O(N**2)
