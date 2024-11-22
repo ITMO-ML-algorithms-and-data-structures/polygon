624af2e98452da765aed7b1276ce3e53d80e8120
Игорь Алексеевич Воробьев <igorrrka963@gmail.com>
2024-11-22 11:20:30 +0300

    2_vorobev_igor_lab3

--- lab2.cpp
+++ lab2.cpp
@@ -2,30 +2,35 @@
 #include<string>
 #include<cassert>
 
-std::string compression(std::string &s) {
-    //функция сжатия
-    // ссылка на s весит 1 байт
+std::string compression(const std::string &s) {
     std::string ans = "";
     if (s.size() == 0) {
+        //сравнение s.size() == 0 за O(1)
         return ans;
     }
-    int cnt_last = 1; // +4 байта
-    ans += s[0];
+    int cnt_last = 1;
+    ans += s[0]; // O(1)
     for (int i=1; i < s.size() ; i++) {
-        // +4 байта из-за цикла
-        // во время выолнения цикла в строке ans максимум 2*m-1 символ, где m - кол-во частей подряд идущих символов
-        // тогда максимальный вес строки достигается на последней итерации и равен 2*m
-        // на последний итерации программа использует больше всего памяти 4 + 4 + 2*n + 1 + 4 + 4 + 2*m = 2*n + 2*m + 17 байт
+        //цикл проходит s.size() - 1 итерацию
+        //сравнение s[i] == ans[ans.size() - 1] за O(1)
         if (s[i] == ans[ans.size() - 1]) {
-            cnt_last += 1;
+            cnt_last += 1; // O(1)
         } else {
-            ans += std::to_string(cnt_last);
-            cnt_last = 1;
-            ans += s[i];
+            // сравнение cnt_last > 1 за O(1)
+            if (cnt_last > 1) {
+                ans += std::to_string(cnt_last); // O(l), где l-кол-во разрядов в числе
+                //суммарное кол-во разрядов во всех числах которые будут преобразованы функцией to_string <= s.size()/2
+                //Тогда в худшем случаи все to_string суммарно работают за O(s.size())
+                cnt_last = 1; // O(1)
+            }
+            ans += s[i]; // O(1)
         }
     }
-    // в конце выполнения функции ans весит 2*m байт
-    ans += std::to_string(cnt_last);
+    if (cnt_last > 1) {
+        // сравнение cnt_last > 1 за O(1)
+        ans += std::to_string(cnt_last); //O(1)
+    }
+    //Итоговая сложность O(s.size())
     return ans;
 }
 
@@ -33,21 +38,23 @@ void test() {
     std::string s1="aaabb";
     assert((compression(s1) == "a3b2"));
     s1="abac";
-    assert((compression(s1) == "a1b1a1c1"));
+    assert((compression(s1) == "abac"));
     s1="DdD";
-    assert((compression(s1) == "D1d1D1"));
+    assert((compression(s1) == "DdD"));
+    s1="";
+    assert((compression(s1) == ""));
 }
 
 int main()
 {
     test();
-    int number_of_lines; // +4 байта
+    int number_of_lines;
     std::cin >> number_of_lines;
-    std::string input; // строка весит n+1 байт, где n - кол-во символов
-    for (int i=0; i < number_of_lines ; i++) {
-        //+4 байта из-за цикла
+    std::string input;
+    while (number_of_lines > 0) {
         std::cin >> input;
         std::cout << compression(input);
+        number_of_lines -= 1;
     }
     return 0;
 }
