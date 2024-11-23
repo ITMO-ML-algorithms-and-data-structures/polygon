d2c283e5701dd39deae419a4fe01494d218cae96
Ilya Yampolskiy <hellboy@niuitmo.ru>
2024-11-24 00:20:43 +0300

    lab_3

--- scratch.cpp
+++ scratch.cpp
@@ -1,21 +1,33 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
 
-using namespace std;
+std::vector<int> null_to_sr_ar(std::vector<int> &arr, const int size) {
+    double srar = 0;
+    int count = 0;
+    int sum = 0;
+    for (int i = 0; i < size; ++i) {
+        if (arr[i] != -1) {
+            count += 1;
+            sum = sum + arr[i];
+            std::cout << sum;
+    srar = sum / count;
+        }
+        for (int i = 0; i < size; ++i) {
+            if (arr[i] == -1) {
+                arr[i] = srar;
+            }
+        }
+    }
+    return arr;
+}
 
-int main(){
-
-    cout << "Size: ";
-    int len; cin >> len;
-
-    vector <double> ass(len);
-
-    cout << "Numbers: ";
-    for (int i = 0; i < len; i++) cin >> ass[i];
-
-    sort(ass.begin(), ass.end());
-
-    if (len % 2 == 0) cout << (ass[len / 2] + ass[len / 2 - 1]) / 2;
-    else cout << ass[len / 2];
+int main() {
+    std::vector<int> arr = {1, -1, 10, 4};
+    const int size = 4;
+        std::vector<int> out = null_to_sr_ar(arr, size);
+        std::cout << "Filled vector: ";
+        for (int i = 0; i < size; ++i) {
+            std::cout << out[i];
+    }
+    return 0;
 }
