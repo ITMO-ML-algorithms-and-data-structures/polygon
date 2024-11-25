abc4724838a0d4b339f6d3978195fe215590de2e
Ilya Yampolskiy <hellboy@niuitmo.ru>
2024-11-25 12:32:40 +0300

    lab_4

--- scratch.cpp
+++ scratch.cpp
@@ -1,33 +1,7 @@
-#include <iostream>
 #include <vector>
+#include <iostream>
 
-std::vector<int> null_to_sr_ar(std::vector<int> &arr, const int size) {
-    double srar = 0;
-    int count = 0;
-    int sum = 0;
-    for (int i = 0; i < size; ++i) {
-        if (arr[i] != -1) {
-            count += 1;
-            sum = sum + arr[i];
-            std::cout << sum;
-    srar = sum / count;
-        }
-        for (int i = 0; i < size; ++i) {
-            if (arr[i] == -1) {
-                arr[i] = srar;
-            }
-        }
-    }
-    return arr;
-}
 
-int main() {
-    std::vector<int> arr = {1, -1, 10, 4};
-    const int size = 4;
-        std::vector<int> out = null_to_sr_ar(arr, size);
-        std::cout << "Filled vector: ";
-        for (int i = 0; i < size; ++i) {
-            std::cout << out[i];
-    }
+int main(){
     return 0;
 }
