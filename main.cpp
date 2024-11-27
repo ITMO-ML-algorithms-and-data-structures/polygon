0b67214b369887990d2587fcfeeed51f916c78cc
Ivan Startsev <aladush3k@mail.ru>
2024-11-27 18:23:51 +0300

    Работа выполнена

--- main.cpp
+++ main.cpp
@@ -1,25 +1,17 @@
 #include <iostream>
 using namespace std;
-// // короче как на егэ забубенить и найти минимальный в матрице
-// int main () {
-//     читаем матрицу
-//     делааем как матрицу
-//     тут минимум задаем
-//     тут тупым циклом переделываем
-//     тут ответ выводим
-//     // надеюс
-//     return 0;
-
 
 class Solution {
 public:
-    int calculateMinimumHP(vector<vector<int>>& dungeon) {
-        int size_n = dungeon.size();
-        int size_m = dungeon[0].size();
+    int calculateMinimumHP(vector<vector<int> > &dungeon) {
+        int size_n = dungeon.size(); // память равна размеру типа числа N и M
+        int size_m = dungeon[0].size(); // т.е N+M = O(1)
         // будем считать необходимое хп обратным проходом, короче как в егэ
-        vector<vector<int>> dp(size_n + 1, vector<int>(size_m + 1, INT_MAX));
-		dp[size_n - 1][size_m] = 1;
+        // Память (N + 1) * (M + 1) * (тип данныХ) = O(N * M)
+        vector<vector<int> > dp(size_n + 1, vector<int>(size_m + 1, INT_MAX));
+        dp[size_n - 1][size_m] = 1;
         dp[size_n][size_m - 1] = 1;
+        // Время N * M = O(N*M)
         for (int i = size_n - 1; i >= 0; i--) {
             for (int j = size_m - 1; j >= 0; j--) {
                 int hp = min(dp[i + 1][j], dp[i][j + 1]) - dungeon[i][j];
@@ -33,3 +25,4 @@ public:
         return dp[0][0];
     }
 };
+// итоговая память и время O(N*M)
