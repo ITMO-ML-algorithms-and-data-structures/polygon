57b61e204a09dc7a1909693ff490bfae0a6895f5
IdarBerbekovJ3110 <ekerrelegend@yandex.ru>
2024-11-29 03:10:26 +0300

    initial commit

---
+++ lab-1/source.cpp
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+//#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+signed main(void){
+	ios::sync_with_stdio(0); cin.tie(0);
+
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+
+	return 0;
+}
