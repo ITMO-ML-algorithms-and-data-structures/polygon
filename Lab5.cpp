896f4e317197c061704cdc22041d93befbd02d45
Finzz71 <timonyakytsk@gmail.com>
2024-11-30 17:36:23 +0300

    Задание 2 - сортировка кучей реализована

--- Lab5.cpp
+++ Lab5.cpp
@@ -32,6 +32,42 @@ vector<int> comb_sort(vector<int>& arr)
 	return arr;
 }
 
+void swap(int& a, int& b) {
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+void heapify(vector<int>& arr, int n, int i) {
+	int largest = i;  
+	int left = 2 * i + 1;  
+	int right = 2 * i + 2;  
+
+	if (left < n && arr[left] > arr[largest])
+		largest = left;
+
+	if (right < n && arr[right] > arr[largest])
+		largest = right;
+
+	if (largest != i) {
+		swap(arr[i], arr[largest]);
+		heapify(arr, n, largest);
+	}
+}
+
+void heapSort(vector<int>& arr) {
+	int n = arr.size();
+
+	for (int i = n / 2 - 1; i >= 0; i--) {
+		heapify(arr, n, i);
+	}
+
+	for (int i = n - 1; i >= 0; i--) {
+		swap(arr[0], arr[i]);
+		heapify(arr, i, 0);
+	}
+}
+
 int main()
 {
 	ifstream input_file("test5.1.txt");
