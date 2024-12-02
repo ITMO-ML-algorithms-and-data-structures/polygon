06c41a7e5c7b3eba0f8f635796db2be5a53153a2
introlina <119130845+introlina@users.noreply.github.com>
2024-12-02 22:05:17 +0300

    code + screen from leetcode

--- lab6_DP_RUSSIAN_DOLL.cpp
+++ lab6_DP_RUSSIAN_DOLL.cpp
@@ -3,55 +3,57 @@
 #include <algorithm>
 using namespace std;
 
-/*ЭТО ЗАДАЧА О ВЛОЖЕНИИ МАТРЁШЕК, hard
-1) ВХОД: МАССИВ ИЗ ПАР (WIDTH, HEIGHT)
-2) НЕЛЬЗЯ МЕНЯТЬ ЭТИ ПАРАМЕТРЫ МЕСТАМИ
-3) НЕОХОДИМО НАЙТИ МАКСИМАЛЬНОЕ КОЛИЧЕСТВО
-МАТРЁШЕК, КОТОРЫЕ МОЖНО СЛОЖИТЬ В ОДНУ 
-(ОТВЕТ С УЧЁТОМ ЭТОЙ ОДНОЙ)*/
 
-bool IfFirstMax(const vector<int>& pair1, const vector<int>& pair2) {
-    return pair1[0] < pair2[0];
-}
-
-bool IfSecondMax(const vector<int>& pair1, const vector<int>& pair2) {
-    return pair1[1] < pair2[1];
-}
+/* Решение задачи о матрёшках сводится к, по сути, двум этапам:
+1) грамотной сортировке: необходимо сортировать width(1 показатель)
+   по возрастанию, а там, где показатели width равны, сортировать
+   height по убыванию, чтобы при дальнейшей обработке не брать дубли в расчёт 
+2) поиск lis(longest increasible subsequense), её макс длина и будет ответом
+*/
 
 
-void SortDolls(vector<vector<int>>& pairs) {
-    sort(pairs.begin(), pairs.end(), IfFirstMax);
-    // stable_sort для сохранения порядка равных элементов
-    stable_sort(pairs.begin(), pairs.end(), IfSecondMax); // O(N*logN), память тоже
-}
-
-int maxPairs(vector<vector<int>>& pairs) { // память под pairs: 8n байт
-    if (pairs.empty()) return 0;
-    SortDolls(pairs);
-    vector<int> dp(pairs.size(), 1); // 4n байт
-    int maxCount = 1; // минимальное кол-во вложенных пар
-
-    // заполняем dp-шку
-    for (int i = 1; i < pairs.size(); ++i) { //O(N)
-        for (int j = 0; j < i; ++j) { // O(N) в худшем случае
-            if (pairs[i][0] > pairs[j][0] && pairs[i][1] > pairs[j][1]) {
-                dp[i] = max(dp[i], dp[j] + 1);
-            }
-        }
-        maxCount = max(maxCount, dp[i]);
+static bool comp(vector<int> &a, vector<int> &b){ // 0(1)
+    if(a[0] == b[0]){
+        return a[1] > b[1];
     }
-    return maxCount;
+    return a[0] < b[0];
 }
+    
+int maxPairs(vector<vector<int>>& pairs) {
+    sort(pairs.begin(), pairs.end(), comp); // O(n*log(n))
+    int i; // 4 байта
+    int j; // 4 байта
+    int len_pairs = pairs.size(); // 4 байта
+    vector<int> lis; // 4*n байт
+    for(i = 0; i < len_pairs; i++){ // O(n)
+        vector<int>::iterator first_height = lower_bound(lis.begin(), lis.end(), pairs[i][1]); // 8 байт
+        // O(log(n))
+        // lower_bound - бинарный поиск (Finds 
+        // the first position in which val (здесь - pairs[i][1]) could be 
+        // inserted without changing the ordering)
+        if (first_height == lis.end()){
+            lis.push_back(pairs[i][1]);
+            }
+        // если итератор указывает на конец
+        else{
+            *first_height = pairs[i][1];
+            // если не на конец, то возвращаем значение на которое указывает итератор
+        }
+    }
+    return lis.size(); // длина lis - ответ на вопрос задачи
+}
+
+// Асимптотическая сложность: O(n * log(n))
+// Память: len_pairs * 8 + 4n + 4*3 = 8n + 4n + 12 = 12n + 12 байт
 
 /*  Общая память примерно 12n(pairs, dp) + 12(i, j, maxCount) байт
     Итоговая сложность O(N * N + NlogN) = O(N^2), где n=N=кол-ву пар*/
 
-/* это пример динамического программирования тк одна большая задача
-разбита на много подзадач (например, мы последовательно рассматриваем 
-каждую матрёшку отдельно, и для неё тоже определяем задачи), также
-используется массив dp, который изменяется в ходе решения. А
-вот алгоритм (сложностью O(N*logN)), написанный мной и хранящийся в файлике lab6_NOT_DP_RussianEnvelopes
-больше смахивает на жадный алгоритм через аналитику */
+/* Итог: написала я классическое DP решение через массив, хранящий максимальные
+кол-ва помещающихся внутрь матрёшек, заполняющийся за O(N^2), как сделала изначально,
+и leetcode не засчитал (огр-е по времени), однако в этом решении тоже
+есть элементы динамического программирования, тому док-во вектор lis меняющийся
+по ходу решения и применение lower_bound для каждой высоты (разбиение на подзадачи)*/
 
 int main() {
     vector<vector<int>> pairs = {{2, 3}, {3, 5}, {4, 4}, {5, 9}, {6, 5}, {7, 6}};
