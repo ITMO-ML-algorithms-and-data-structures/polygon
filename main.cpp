17ba11313605b2f77e6b766df6d4b9bc4c3a5763
VherediaTomas <tomasvargasherdia@gmail.com>
2024-12-03 02:08:14 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,100 +1,82 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 #include <unordered_set>
+
 using namespace std;
 
-const int MAX_VALUE = 100000;//4 bytes
-
-vector<int> eliminarDuplicados(int arr[], int size) {
-    vector<bool> encontrados(MAX_VALUE + 1, false); //vector, max+1 because we have to compare the 0 position, we start every position with false
-    //MAX_VAlUE / 8 bytes
-    vector<int> unicos;//24 bytes
-
-
-    for (int i = 0;i < size;i++) {
-        //4 bytes for i
-        int ing = arr[i];
-        //4 bytes for ing
-
-        if(ing<0||ing>MAX_VALUE){
-            throw out_of_range("dont allowed number: " + to_string(ing));
+bool isValid(const vector<pair<int, int>>& subset) { //made a bool with that parameters
+    unordered_set<int> used; // we made an unordered called used //we save here every number that already apears in subset
+    for (const auto& p : subset) { //we made a bucle with better logic
+        if (used.count(p.first) || used.count(p.second)) {// verify p in the first posicion or the second one in the used group
+            return false;// if it already any of them already exist subset is not valid
         }
-
-        if (!encontrados[ing]) { // we compare encontrados with ing, and change the logic
-            encontrados[ing] = true; // to save in the memory that we found that number
-            unicos.push_back(ing); // add that number to unicos
-            //every int use 4 bytes
-        }
-
-
+        used.insert(p.first);//if not exist we add to used
+        used.insert(p.second);//if not exist we add to used
     }
-
-    return unicos;
-
-    /* Imprimir los elementos únicos
-    cout << "Without doubles: ";
-    for (int num : unicos) {
-        cout << num << " ";
-    }
-    cout << endl;*/
-}
-
-/*int main() {
-
-    //dataset
-    int arr[] = {1, 2, 3, 1, 100000, 100000};
-    int size = 5;
-
-    //test
-   vector <int> unicos = eliminarDuplicados(arr, size);
-
-    // Imprimir los elementos únicos
-    cout << "Without doubles: ";
-    for (int num : unicos) {
-        cout << num << " ";
-    }
-    cout << endl;
-
-    return 0;
-}*/
-
-int main(){
-
-    //common test
-    int  arr1[]={1,2,3,3,5,6,6};
-    vector<int> resultado1= eliminarDuplicados(arr1,7);
-    cout<<"test1:  ";
-    for(int num : resultado1)
-        cout << num << " ";
-    cout<<endl;
-
-
-
-    //test with empty
-    int arr2[]={};
-    vector<int> resultado2 = eliminarDuplicados(arr2,0);
-    cout << "test2:  ";
-    for (int num : resultado2)
-        cout << num <<" ";
-    cout<<endl;
-
-    //test with limit case
-    int arr3[]={0,MAX_VALUE,0};
-    vector<int> resultadao3= eliminarDuplicados(arr3,3);
-    cout << "test3:  ";
-    for (int num:resultadao3)
-        cout << num << " ";
-    cout << endl;
-
-    //error test
-    int arr4[]={-1,1};
-    vector<int> resultados4= eliminarDuplicados(arr4,2);
-    cout<<"test4:  ";
-    for(int num:resultados4)
-        cout<<num<<" ";
-    cout<<endl;
-
-    return 0;
+    return true;//we show that the subconj is valid
 }
 
 
+//we made a vector with to find the  pair that works or a single element if it doesnt work
+//we need a vector with pairs
+vector<vector<int>> findMaxMatching(const vector<pair<int, int>>& pairs) {
+    int n = pairs.size();//the size of pairs save in n
+    vector<pair<int, int>> maxSubset; // vector to save the final exit
+
+
+    for (int mask = 0; mask < (1<<n); ++mask) { //
+        vector<pair<int, int>> subset;
+        for (int i = 0; i < n; ++i) {
+            if (mask & (1<<i)) {
+                subset.push_back(pairs[i]);
+            }
+        }
+        if (isValid(subset) && subset.size() > maxSubset.size()) {
+            maxSubset = subset;
+        }
+    }
+
+
+    unordered_set<int> included;
+    for (const auto& p : maxSubset) {
+        included.insert(p.first);
+        included.insert(p.second);
+    }
+
+    unordered_set<int> singlesSet;
+    for (const auto& p : pairs) {
+        if (!included.count(p.first)) singlesSet.insert(p.first);
+        if (!included.count(p.second)) singlesSet.insert(p.second);
+    }
+
+
+    vector<vector<int>> result;
+    for (const auto& p : maxSubset) {
+        result.push_back({p.first, p.second});
+    }
+    for (int single : singlesSet) {
+        result.push_back({single});
+    }
+
+    return result;
+}
+
+int main() {
+
+    vector<pair<int, int>> pairs = {{3, 2}, {2, 6}, {2, 4},{6,5},{5,2},{1,7}};
+
+    vector<vector<int>> result = findMaxMatching(pairs);
+
+    cout << "Maximum agreement:" << endl;
+    for (const auto& group : result) {
+        cout << "(";
+        for (size_t i = 0; i < group.size(); ++i) {
+            cout << group[i];
+            if (i + 1 < group.size()) cout << ", ";
+        }
+        cout << ")" << endl;
+    }
+
+    return 0;
+}
