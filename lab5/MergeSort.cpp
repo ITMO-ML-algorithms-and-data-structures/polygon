f6d88b7d4e0cc3becdc8cfbc7ead20868e6bac97
Vitaly_Ziryanov <vittzvar@gmail.com>
2024-12-06 12:34:11 +0300

    Add some changes for lab5

--- lab5/MergeSort.cpp
+++ lab5/MergeSort.cpp
@@ -2,15 +2,19 @@
 #include <vector>
 #include <random>
 #include <chrono>
+#include <cassert>
 
 
+// Слияние двух массивов
 void merge(std::vector<int>& arr, const int& left, const int& mid, const int& right) {
+    // Длина левого и правого массивов
     int nLeft = mid - left + 1;
     int nRight = right - mid; 
 
     std::vector<int> arrLeft(nLeft);
     std::vector<int> arrRight(nRight);
-
+    
+    // Создаём массивы левой и правой частей массива
     for (int i=0; i<nLeft; i++) arrLeft[i] = arr[left + i];
     for (int i=0; i<nRight; i++) arrRight[i] = arr[mid + 1 + i];
 
@@ -18,6 +22,7 @@ void merge(std::vector<int>& arr, const int& left, const int& mid, const int& ri
     int j = 0;
     int k = left;
 
+    // Слияние двух отсортированных массива в один
     while (i<nLeft && j<nRight) {
         if (arrLeft[i]<=arrRight[j]) {
             arr[k] = arrLeft[i];
@@ -29,6 +34,7 @@ void merge(std::vector<int>& arr, const int& left, const int& mid, const int& ri
         k++;
     }
 
+    // Добавояем отсавшиеся эл-ты в итоговый массив
     while (i<nLeft) {
         arr[k] = arrLeft[i];
         i++;
@@ -43,6 +49,9 @@ void merge(std::vector<int>& arr, const int& left, const int& mid, const int& ri
 }
 
 
+// Сортировка слиянием
+// Использование памяти: O(n) (т. к создаём 2 массива в общей длины n)
+// Сложность алгоритма: O(n log n) (рекурсивно разбиваем log n и выполняем слияние O(n))
 std::vector<int> mergeSort(std::vector<int> arr, int left, int right) {
     if (left<right) {
         int mid = left + (right - left) / 2;
@@ -56,22 +65,46 @@ std::vector<int> mergeSort(std::vector<int> arr, int left, int right) {
 }
 
 
+// Функция для создания случайного массива
 std::vector<int> arrRand(const int& n, const int& arrMin, const int& arrMax) {
     std::vector<int> arr(n);
     std::random_device rd;
     std::mt19937 seed(rd());
+    arr[0] = arrMax;
+    arr[n - 1] = arrMin;
 
-    for (int i=0; i<n; ++i) arr[i] = arrMin + seed() % (arrMax - arrMin);
+    for (int i=1; i<n-1; ++i) arr[i] = arrMin + seed() % (arrMax - arrMin);
 
     return arr;
 }
 
 
-void display(const std::vector<int>& arr, const std::vector<int>& arrSort, std::chrono::duration<double> duration) {
-    int n = arr.size();
-    std::cout << "\nРазмер массива:\n" << n;
+std::vector<int> arrWorse(const int& n) {
+    std::vector<int> arr(n);
 
-    std::cout << "\nВремя выполнения:\n" << duration.count();
+    for (int i=0; i<n; ++i) arr[i] = n - i;
+
+    return arr;
+}
+
+
+std::vector<int> arrBett(const int& n) {
+    std::vector<int> arr(n);
+
+    for (int i=0; i<n; ++i) arr[i] = i;
+
+    return arr;
+}
+
+
+// Вывод
+void display(const std::vector<int>& arr, const std::vector<int>& arrSort, std::chrono::duration<double> duration) {
+    /*
+    int n = arr.size();
+    std::cout << "\nРазмер массива:" << n;
+    */
+
+    std::cout << duration.count() << ", ";
     /*
     std::cout << "\nИсходный массив:\n";
     for (int i = 0; i < n; i++) std::cout << arr[i] << " ";
@@ -82,11 +115,24 @@ void display(const std::vector<int>& arr, const std::vector<int>& arrSort, std::
 }
 
 
+// Проверка на корректность сортировки
+bool check(const std::vector<int>& arr) {
+    int n = arr.size();
+    for (int i=1; i<n; ++i) {
+        if (arr[i - 1]>arr[i]) return false;
+    }
+
+    return true;
+}
+
+
+// Тест для алгоритма сортировки
 void test(const int& n = 10, const int& arrMax = 100, const int& arrMin = 0) {
     std::vector<int> arr = arrRand(n, arrMin, arrMax);
 
     auto start = std::chrono::high_resolution_clock::now();
     std::vector<int> arrSort = mergeSort(arr, 0, n - 1);
+    assert(check(arrSort));
     auto end = std::chrono::high_resolution_clock::now();
 
     std::chrono::duration<double> duration = end - start;
@@ -95,6 +141,7 @@ void test(const int& n = 10, const int& arrMax = 100, const int& arrMin = 0) {
 
 
 int main() {
-    test();
+    for (int i=1000; i <= 50000; i+=1000)
+        test(i, 999999, 0);
     return 0;
 }
--- lab5/RadixSort.cpp
+++ lab5/RadixSort.cpp
@@ -3,18 +3,23 @@
 #include <random>
 #include <chrono>
 #include <algorithm>
+#include <cassert>
 
 
+// Функция для сортировки подсчётом на основе текущего разряда
 void countingSort(std::vector<int>& arr, const int& exp) {
     int n = arr.size();
     std::vector<int> res(n);
-    int count[10] = {0};
+    int count[10] = {0}; // Массив для подсчёта кол-во цифр 0-9 
 
-    for (int i = 0; i < n; i++) count[(arr[i] / exp) % 10]++;
-
-    for (int i = 1; i < 10; i++) count[i] += count[i - 1];
-
-    for (int i = n - 1; i >= 0; i--) {
+    // Подсчёт появленме разряда
+    for (int i=0; i<n; i++) count[(arr[i] / exp) % 10]++;
+    
+    // Чтобы count[i] содержал индекс разряда в итоговом массиве
+    for (int i=1; i<10; i++) count[i] += count[i - 1];
+    
+    // Итоговый массив
+    for (int i=n-1; i>=0; i--) {
         res[count[(arr[i] / exp) % 10] - 1] = arr[i];
         count[(arr[i] / exp) % 10]--;
     }
@@ -23,31 +28,61 @@ void countingSort(std::vector<int>& arr, const int& exp) {
 }
 
 
+// Подразрядная сортировка
+// Использование памяти: O(n * r) r - разряд числа
+// Сложность алгоритма: O(n * r) r - разряд числа
 std::vector<int> radixSort(std::vector<int> arr) {
+    // Максимальный эл в массиве
     int arrMax = *max_element(arr.begin(), arr.end());
 
-    for (int exp = 1; arrMax / exp > 0; exp *= 10) countingSort(arr, exp);
+    // Сортировка для каждого разряда
+    for (int exp=1; arrMax/exp>0; exp*=10) countingSort(arr, exp);
 
     return arr;
 }
 
 
+// Функция для создания случайного массива
 std::vector<int> arrRand(const int& n, const int& arrMin, const int& arrMax) {
     std::vector<int> arr(n);
     std::random_device rd;
     std::mt19937 seed(rd());
+    arr[0] = arrMax;
+    arr[n - 1] = arrMin;
 
-    for (int i=0; i<n; ++i) arr[i] = arrMin + seed() % (arrMax - arrMin);
+    for (int i=1; i<n-1; ++i) arr[i] = arrMin + seed() % (arrMax - arrMin);
 
     return arr;
 }
 
 
-void display(const std::vector<int>& arr, const std::vector<int>& arrSort, std::chrono::duration<double> duration) {
-    int n = arr.size();
-    std::cout << "\nРазмер массива:\n" << n;
+std::vector<int> arrWorse(const int& n) {
+    std::vector<int> arr(n);
 
-    std::cout << "\nВремя выполнения:\n" << duration.count();
+    for (int i=0; i<n; ++i) arr[i] = std::pow(10, i);
+
+    return arr;
+}
+
+
+std::vector<int> arrBett(const int& n) {
+    std::vector<int> arr(n);
+
+    for (int i=0; i<n; ++i) arr[i] = i;
+
+    return arr;
+}
+
+
+
+// Вывод
+void display(const std::vector<int>& arr, const std::vector<int>& arrSort, std::chrono::duration<double> duration) {
+    /*
+    int n = arr.size();
+    std::cout << "\nРазмер массива:" << n;
+    */
+
+    std::cout << duration.count() << ", ";
     /*
     std::cout << "\nИсходный массив:\n";
     for (int i = 0; i < n; i++) std::cout << arr[i] << " ";
@@ -58,11 +93,24 @@ void display(const std::vector<int>& arr, const std::vector<int>& arrSort, std::
 }
 
 
+// Проверка на корректность сортировки
+bool check(const std::vector<int>& arr) {
+    int n = arr.size();
+    for (int i=1; i<n; ++i) {
+        if (arr[i - 1]>arr[i]) return false;
+    }
+
+    return true;
+}
+
+
+// Тест для алгоритма сортировки
 void test(const int& n = 10, const int& arrMax = 100, const int& arrMin = 0) {
-    std::vector<int> arr = arrRand(n, arrMin, arrMax);
+    std::vector<int> arr = arrRand(n, arrMax, arrMin);
 
     auto start = std::chrono::high_resolution_clock::now();
     std::vector<int> arrSort = radixSort(arr);
+    assert(check(arrSort));
     auto end = std::chrono::high_resolution_clock::now();
 
     std::chrono::duration<double> duration = end - start;
@@ -71,6 +119,7 @@ void test(const int& n = 10, const int& arrMax = 100, const int& arrMin = 0) {
 
 
 int main() {
-    test(10, 999999, 0);
+    for (int i=1000; i <= 50000; i+=1000)
+        test(i, 999999, 0);
     return 0;
 }
