13d68e9def1617156dfa15e68cc5a03a02d20b3e
wxn0zq <vikazyeva2017@gmail.com>
2024-12-09 09:18:06 +0300

    lab-3

--- lab-3.cpp
+++ lab-3.cpp
@@ -1,29 +1,42 @@
 #include <iostream>
 #include <string>
+#include <cassert>
 using namespace std;
 
-string compressString(const string& str) {
-    if (str.empty()) return "";
+string compress(string str) { //Объявляется функция compress,чтобы избежать копирования строки
+    if (str.empty()) return ""; // O(1) Проверяется, пуста ли строка. Если строка пуста, функция сразу возвращает пустую строку 
 
-    string result;
-    int count = 1;
+    string result; //  O(1)  Объявляется строка result, которая будет хранить результат сжатой строки
+    int count = 1;//  O(1)  Переменная для подсчета количества повторений каждого символа подряд
 
-    for (size_t i = 1; i <= str.size(); ++i) {
-        if (i == str.size() || str[i] != str[i - 1]) {
-            result += str[i - 1];
-            result += to_string(count);
-            count = 1;
+    for (size_t i = 1; i <= str.size(); ++i) { // O(n)  цикл по символам строки
+        if (i == str.size() || str[i] != str[i - 1]) { // O(1)
+            result += str[i - 1]; //   O(1)  добавляется последний символ из последовательности повторяющихся символов
+            result += to_string(count); // O(1) Добавляется количество повторений данного символа в виде строки 
+            count = 1; // O(1)
         } 
         else {
-            ++count;
+            ++count; // O(1)
             }
     }
     return result;
 }
 
+void Test(){
+    string test_1 = "a3b2z5y3w5h3i4";
+    assert(compress("aaabbzzzzzyyywwwwwhhhiiii") == test_1);
+    std::cout<<"OK";
+
+    string test_2 = "a2b2c2";
+    assert(compress("aabbcc") == test_2);
+    std::cout<<"OK";
+}
+
+
 int main() {
     string input = "aaabbzzzzzyyywwwwwhhhiiii";
-    string compressed = compressString(input);
-    cout << compressed << endl;
+    string compressed = compress(input); //Вызывается функция compress, которая сжимает строку input, и результат сохраняется в переменную compressed
+    cout << compressed << endl; //Вывод результата
+    Test();
 }
 
