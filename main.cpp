f6f16c4a661a264edb1937dd2819510eab1074c8
Ivan Startsev <aladush3k@mail.ru>
2024-12-13 11:19:20 +0300

    ready 1

--- main.cpp
+++ main.cpp
@@ -4,7 +4,9 @@ using namespace std;
 class Solution {
     public:
         int minNumberOperations(vector<int>& target) {
+            // память короче немного
             int answer = target[0];
+            // сложность цикла O(N)
             for (int i = 1; i < target.size(); i++) {
                 if (target[i] > target[i - 1]) {
                     answer += target[i] - target[i - 1];
@@ -13,3 +15,4 @@ class Solution {
             return answer;
     }
 };
+// итого память O(1), время O(N)
