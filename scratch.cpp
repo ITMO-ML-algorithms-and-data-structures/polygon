7f63598fc9e6844c4073ff8a7da33df9efafc709
Ilya Yampolskiy <hellboy@niuitmo.ru>
2024-12-17 22:07:34 +0300

    lab_5

--- scratch.cpp
+++ scratch.cpp
@@ -1,6 +1,7 @@
 #include <chrono>
 #include <vector>
 #include <iostream>
+#include <algorithm>
 void bubble_sort(int arr[], const int size){
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size - 1; j++){
@@ -10,10 +11,10 @@ void bubble_sort(int arr[], const int size){
         }
     }
 }
-void smooth_sort(int arr[], const int size) {
-    for (int i = 0; i < size - 1; i++) {
+void selection_sort(std::vector<int>& arr) {
+    for (int i = 0; i < arr.size() - 1; ++i) {
         int min_ind = i;
-        for (int j = i + 1; j < size; j++) {
+        for (int j = i + 1; j < arr.size(); ++j) {
             if (arr[j] < arr[min_ind]) {
                 min_ind = j;
             }
@@ -23,12 +24,34 @@ void smooth_sort(int arr[], const int size) {
         }
     }
 }
-
+void bucket_sort(std::vector<int>& arr) {
+    int min_val = arr[0];
+    int max_val = arr[0];
+    for (int i = 1; i < arr.size(); ++i) {
+        if (arr[i] < min_val) {
+            min_val = arr[i];
+        }
+        if (arr[i] > max_val) {
+            max_val = arr[i];
+        }
+    }
+    int num_buckets = max_val - min_val + 1;
+    std::vector<std::vector<int>> buckets(num_buckets);
+    for (int i = 0; i < arr.size(); i++) {
+        int num = arr[i];
+        buckets[num - min_val].push_back(num);
+    }
+    arr.clear();
+    for (auto bucket : buckets) {
+        selection_sort(bucket);
+        arr.insert(arr.end(), bucket.begin(), bucket.end());
+    }
+}
 int main(){
     int arr[] = {4, 2, 1, 3, 5};
     int arr1[] = {4, 2, 1, 3, 5};
-    int arr2[] = {4, 2, 1, 3, 5};
-    int arr3[] = {4, 2, 1, 3, 5};
+    std::vector<int> arr2 = {4, 2, 1, 3, 5};
+    std::vector<int> arr3 = {4, 2, 1, 3, 5};
     int size = sizeof(arr) / sizeof(arr[0]);
     auto start1 = std::chrono::high_resolution_clock::now();
     bubble_sort(arr1, size);
@@ -40,8 +63,8 @@ int main(){
     std::chrono::duration<double> elapsed_time1 = end1 - start1;
     std::cout << "Время выполнения: " << elapsed_time1.count() << " секунд" << std::endl;
     auto start2 = std::chrono::high_resolution_clock::now();
-     smooth_sort(arr2, size);
-    std::cout << "Отсортированный плавно массив:" << std::endl;
+    selection_sort(arr2);
+    std::cout << "Отсортированный выбором массив:" << std::endl;
     for (int i = 0; i < size; i++){
         std::cout << arr2[i] << std::endl;
     }
@@ -49,7 +72,7 @@ int main(){
     std::chrono::duration<double> elapsed_time2 = end2 - start2;
     std::cout << "Время выполнения: " << elapsed_time2.count() << " секунд" << std::endl;
     auto start3 = std::chrono::high_resolution_clock::now();
-    bucket_sort(arr3, size);
+    bucket_sort(arr3);
     std::cout << "Отсортированный блочно массив:" << std::endl;
     for (int i = 0; i < size; i++){
         std::cout << arr3[i] << std::endl;
