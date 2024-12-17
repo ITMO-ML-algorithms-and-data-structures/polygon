c6f6826ad39dee6e69a90077255c00d6214d7c8d
Ilya Yampolskiy <hellboy@niuitmo.ru>
2024-12-18 01:48:32 +0300

    lab_5

--- scratch.cpp
+++ scratch.cpp
@@ -2,10 +2,12 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
-void bubble_sort(int arr[], const int size){
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size - 1; j++){
-            if (arr[j] > arr[j + 1]){
+#include <unordered_set>
+#include <random>
+void bubble_sort(std::vector<int>& arr) {
+    for (int i = 0; i < arr.size(); i++) {
+        for (int j = 0; j < arr.size() - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
                 std::swap(arr[j], arr[j + 1]);
             }
         }
@@ -48,34 +50,43 @@ void bucket_sort(std::vector<int>& arr) {
     }
 }
 int main(){
-    int arr[] = {4, 2, 1, 3, 5};
-    int arr1[] = {4, 2, 1, 3, 5};
-    std::vector<int> arr2 = {4, 2, 1, 3, 5};
-    std::vector<int> arr3 = {4, 2, 1, 3, 5};
-    int size = sizeof(arr) / sizeof(arr[0]);
+    std::vector<int> arr_rand;
+    std::unordered_set<int> used;
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distrib(1, 10);
+
+    while (arr_rand.size() < 10) {
+        int num = distrib(gen);
+        if (used.find(num) == used.end()) {
+            arr_rand.push_back(num);
+            used.insert(num);
+        }
+    }
+    int size_rand = 10;
     auto start1 = std::chrono::high_resolution_clock::now();
-    bubble_sort(arr1, size);
+    bubble_sort(arr_rand);
     std::cout << "Отсортированный пузырьком массив:" << std::endl;
-    for (int i = 0; i < size; i++){
-        std::cout << arr1[i] << std::endl;
+    for (int i = 0; i < size_rand; i++){
+        std::cout << arr_rand[i] << std::endl;
     }
     auto end1 = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed_time1 = end1 - start1;
     std::cout << "Время выполнения: " << elapsed_time1.count() << " секунд" << std::endl;
     auto start2 = std::chrono::high_resolution_clock::now();
-    selection_sort(arr2);
+    selection_sort(arr_rand);
     std::cout << "Отсортированный выбором массив:" << std::endl;
-    for (int i = 0; i < size; i++){
-        std::cout << arr2[i] << std::endl;
+    for (int i = 0; i < size_rand; i++){
+        std::cout << arr_rand[i] << std::endl;
     }
     auto end2 = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed_time2 = end2 - start2;
     std::cout << "Время выполнения: " << elapsed_time2.count() << " секунд" << std::endl;
     auto start3 = std::chrono::high_resolution_clock::now();
-    bucket_sort(arr3);
+    bucket_sort(arr_rand);
     std::cout << "Отсортированный блочно массив:" << std::endl;
-    for (int i = 0; i < size; i++){
-        std::cout << arr3[i] << std::endl;
+    for (int i = 0; i < size_rand; i++){
+        std::cout << arr_rand[i] << std::endl;
     }
     auto end3 = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed_time3 = end3 - start3;
